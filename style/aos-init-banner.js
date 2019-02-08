dcffe24affa3fe0f3499995298d99275cd92a5c4
Cruz Godar <cruz.godar@gmail.com>
2019-02-08 18:23:18 +0100

    Rolled out new banner animation

--- style/aos-init-banner.js
+++
@@ -1,9 +0,0 @@
-//Initiate anamations when they're a third of the way up the screen.
-
-var w = window,
-d = document,
-e = d.documentElement,
-g = d.getElementsByTagName('body')[0],
-y = w.innerHeight|| e.clientHeight|| g.clientHeight;
-
-AOS.init({duration: 1200, once: false, offset: y/3});
