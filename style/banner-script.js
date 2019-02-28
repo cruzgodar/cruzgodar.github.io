63d4f47aac973c334152dc40aa76242e5888120c
Cruz Godar <cruz.godar@gmail.com>
2019-02-27 20:39:10 -0800

    I definitely did something, I'm just not sure what

--- style/banner-script.js
+++ style/banner-script.js
@@ -15,27 +15,13 @@ $(function()
 		if (scroll > 0)
 		{
 			$(".banner").css("opacity", .5 + .5 * Math.sin(Math.PI * Math.max(1 - scroll / y, 0) - .5 * Math.PI));
-		}
-		
-		
-		
-		if (scroll > 10)
-		{
-			//Fade out scroll button
-			$(".scroll-button").stop();
-			$(".scroll-button").animate({opacity: 0}, 200, "swing");
-		}
-		
-		else
-		{
-			//Fade in scroll button
-			$(".scroll-button").stop();
-			$(".scroll-button").animate({opacity: 1}, 200, "swing");
+			
+			$(".scroll-button").css("opacity", .5 + .5 * Math.sin(Math.PI * Math.max(1 - 3 * scroll / y, 0) - .5 * Math.PI));
 		}
 	});
 	
 	$('.scroll-button').click(function()
 	{
-		$([document.documentElement, document.body]).animate({scrollTop: $("#content").offset().top}, 600, "swing");
+		$([document.documentElement, document.body]).animate({scrollTop: $("#content").offset().top}, 900, "swing");
 	});
 });
