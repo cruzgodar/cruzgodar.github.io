ca45d8851417fa4cc0fbe3cf3b3cc0ca76f62243
Cruz Godar <cruz.godar@gmail.com>
2019-02-27 18:25:53 -0800

    Added scroll button

--- style/banner-script.js
+++ style/banner-script.js
@@ -10,11 +10,32 @@ $(function()
 		g = d.getElementsByTagName('body')[0],
 		y = w.innerHeight|| e.clientHeight|| g.clientHeight;
 
-		scroll = $(this).scrollTop()
+		scroll = $(this).scrollTop();
 		
 		if (scroll > 0)
 		{
 			$(".banner").css("opacity", .5 + .5 * Math.sin(Math.PI * Math.max(1 - scroll / y, 0) - .5 * Math.PI));
 		}
+		
+		
+		
+		if (scroll > 10)
+		{
+			//Fade out scroll button
+			$(".scroll-button").stop();
+			$(".scroll-button").animate({opacity: 0}, 200, "swing");
+		}
+		
+		else
+		{
+			//Fade in scroll button
+			$(".scroll-button").stop();
+			$(".scroll-button").animate({opacity: 1}, 200, "swing");
+		}
+	});
+	
+	$('.scroll-button').click(function()
+	{
+		$([document.documentElement, document.body]).animate({scrollTop: $("#content").offset().top}, 600, "swing");
 	});
 });
