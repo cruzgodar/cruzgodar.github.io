d60f4546bd26538c1258668055adfb23d1bc3787
Cruz Godar <cruz.godar@gmail.com>
2019-02-28 12:33:58 -0800

    Testing no-touch class for mobile devices

--- style/aos-init.js
+++ style/aos-init.js
@@ -1,4 +1,4 @@
-//Initiate anamations when they're a third of the way up the screen.
+//Initiate anamations when they're a third of the way up the screen. Also holds some other stuff that's required on page load.
 
 var w = window,
 d = document,
@@ -7,3 +7,10 @@ g = d.getElementsByTagName('body')[0],
 y = w.innerHeight|| e.clientHeight|| g.clientHeight;
 
 AOS.init({duration: 1200, once: true, offset: y/3});
+
+
+
+if ("ontouchstart" in document)
+{
+    Y.one("body").removeClass("no-touch");
+}
