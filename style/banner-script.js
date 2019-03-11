50f14e463b486ba472e96a887ab4fc9dadfe60a7
Cruz Godar <cruz.godar@gmail.com>
2019-03-10 23:28:06 -0700

    Tested scroll on page load so that scroll-dependent things like the background don't show on load

--- style/banner-script.js
+++ style/banner-script.js
@@ -2,6 +2,29 @@
 
 $(function()
 {
+	//Test scroll position on page load so that the background and other scroll-dependent things don't show on refresh.
+	var w = window,
+	d = document,
+	e = d.documentElement,
+	g = d.getElementsByTagName("body")[0],
+	y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+	
+	scroll = $(this).scrollTop();
+	
+	if (scroll >= 0)
+	{
+		$("#background-image").css("opacity", .5 + .5 * Math.sin(Math.PI * Math.max(1 - scroll / y, 0) - .5 * Math.PI));
+		
+		if (scroll <= y/2)
+		{
+			$(".scroll-button").css("opacity", .5 + .5 * Math.sin(Math.PI * Math.max(1 - 3 * scroll / y, 0) - .5 * Math.PI));
+			
+			$(".name-text").css("opacity", .5 + .5 * Math.sin(Math.PI * Math.max(1 - 3 * scroll / y, 0) - .5 * Math.PI));
+		}
+	}
+	
+	
+	
 	$(window).scroll(function()
 	{
 		var w = window,
@@ -9,7 +32,7 @@ $(function()
 		e = d.documentElement,
 		g = d.getElementsByTagName("body")[0],
 		y = w.innerHeight|| e.clientHeight|| g.clientHeight;
-
+	
 		scroll = $(this).scrollTop();
 		
 		if (scroll >= 0)
