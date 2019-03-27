e55f0c2c993366e425525747c6b6dbc37ca38f94
Cruz Godar <cruz.godar@gmail.com>
2019-03-26 21:35:54 -0700

    Fixed bugs on page load

--- style/banner-script.js
+++ style/banner-script.js
@@ -1,72 +1,16 @@
+var banner_done = 0;
+var scroll_button_done = 0;
+
 $(function()
-{	
-	var banner_done = 0;
-	var scroll_button_done = 0;
-	
+{
 	AOS.init({duration: 1200, once: false, offset: y/3});
 	
 	$(window).scroll(function()
 	{
-		var scroll = $(this).scrollTop();
-		var opacity = 0;
-		
-		if (scroll >= 0)
-		{
-			if (scroll <= y)
-			{
-				opacity = .5 + .5 * Math.sin(Math.PI * Math.max(1 - scroll / y, 0) - .5 * Math.PI);
-				$("#background-image").css("opacity", opacity);
-				
-				if (opacity == 0)
-				{
-					banner_done = 1;
-				}
-				
-				else
-				{
-					banner_done = 0;
-				}
-			}
-			
-			else if (banner_done == 0)
-			{
-				$("#background-image").css("opacity", 0);
-				banner_done = 1;
-			}
-			
-			
-			
-			if (scroll <= y/3)
-			{
-				opacity = .5 + .5 * Math.sin(Math.PI * Math.max(1 - 3 * scroll / y, 0) - .5 * Math.PI);
-				
-				$(".scroll-button").css("opacity", opacity);
-				
-				$(".name-text").css("opacity", opacity);
-				
-				if (opacity == 0)
-				{
-					scroll_button_done = 1;
-				}
-				
-				else
-				{
-					scroll_button_done = 0;
-				}
-			}
-			
-			else if (scroll_button_done == 0)
-			{
-				$(".scroll-button").css("opacity", 0);
-				
-				$(".name-text").css("opacity", 0);
-				
-				scroll_button_done = 1;
-			}
-		}
+		scroll_update();
 	});
 	
-	//$(window).scroll();
+	scroll_update();
 	
 	
 	
@@ -85,3 +29,66 @@ $(function()
 		$("#background-image")[0].offsetHeight;
 	});
 });
+
+
+
+function scroll_update()
+{
+	var scroll = $(window).scrollTop();
+	var opacity = 0;
+	
+	if (scroll >= 0)
+	{
+		if (scroll <= y)
+		{
+			opacity = .5 + .5 * Math.sin(Math.PI * Math.max(1 - scroll / y, 0) - .5 * Math.PI);
+			$("#background-image").css("opacity", opacity);
+			
+			if (opacity == 0)
+			{
+				banner_done = 1;
+			}
+			
+			else
+			{
+				banner_done = 0;
+			}
+		}
+		
+		else if (banner_done == 0)
+		{
+			$("#background-image").css("opacity", 0);
+			banner_done = 1;
+		}
+		
+		
+		
+		if (scroll <= y/3)
+		{
+			opacity = .5 + .5 * Math.sin(Math.PI * Math.max(1 - 3 * scroll / y, 0) - .5 * Math.PI);
+			
+			$(".scroll-button").css("opacity", opacity);
+			
+			$(".name-text").css("opacity", opacity);
+			
+			if (opacity == 0)
+			{
+				scroll_button_done = 1;
+			}
+			
+			else
+			{
+				scroll_button_done = 0;
+			}
+		}
+		
+		else if (scroll_button_done == 0)
+		{
+			$(".scroll-button").css("opacity", 0);
+			
+			$(".name-text").css("opacity", 0);
+			
+			scroll_button_done = 1;
+		}
+	}
+}
