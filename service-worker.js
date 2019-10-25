f84324fa18d8f58cb26320d3f30d557b6f745da5
Cruz Godar <cruz.godar@gmail.com>
2019-10-25 07:11:15 -0700

    Rewrote service worker using Workbox

--- service-worker.js
+++ service-worker.js
@@ -1,82 +1,30 @@
-var CACHE_NAME = "static-cache";
-var urlsToCache =
-[
-    "./",
-    "./index.html",
-    "./home/home.html",
-    "./index.html?page=%2Fhome%2Fhome.html",
-    "./home/images.json",
-    
-    "./style/css-bundle.min.css",
-    "./style/aos.min.css",
-    
-    "./scripts/js-bundle.min.js",
-    "./scripts/aos.min.js",
-    
-    "./home/style/home.min.css",
-    "./home/scripts/home.min.js"
-];
+importScripts("/scripts/workbox-sw.min.js");
 
-self.addEventListener("install", function(event)
+if (workbox)
 {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        
-        .then(function(cache)
-        {
-            return cache.addAll(urlsToCache);
-        })
-    );
-});
-
-
-
-self.addEventListener("fetch", function(event)
-{
-    event.respondWith(
-        caches.match(event.request)
-        
-        .then(function(response)
-        {
-            return response || fetchAndCache(event.request);
-        })
-    );
-});
-
-function fetchAndCache(url)
-{
-    return fetch(url)
-    
-    
-    
-    .then(function(response)
-    {
-        //Check if we received a valid response
-        if (!response.ok)
-        {
-            console.log("Invalid response: " + response.statusText);
-        }
-        
-        return response;
-    })
-    
-    
-    
-    .then(function(response)
-    {
-        return caches.open(CACHE_NAME)
-        
-        .then(function(cache)
-        {
-            cache.put(url, response.clone());
-            return response;
-        });
-    })
-    
-    
-    
-    .catch(function(error)
-    {
-        console.log("Request failed: ", error);
-    });
+	console.log("Workbox loaded successfully");
 }
+
+
+//By default, go directly to the network. We want the latest versions of everything.
+workbox.routing.registerRoute(
+	/.+/,
+	new workbox.strategies.NetworkFirst()
+);
+
+//For image files, though, we'll cache them for a week. It's not as important to have the latest versions, and they are by far the biggest use of the network.
+workbox.routing.registerRoute(
+	/\.(?:webp|png|jpg|jpeg|gif)$/,
+	new workbox.strategies.CacheFirst({
+		cacheName: "image-cache",
+		plugins: [
+			new workbox.expiration.Plugin({
+				//Cache up to 200 images.
+				maxEntries: 200,
+				
+				//Cache for a maximum of a week.
+				maxAgeSeconds: 7 * 24 * 60 * 60,
+			})
+		],
+	})
+);
