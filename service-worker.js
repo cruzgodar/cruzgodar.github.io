51e78a1a183f4444ef219521a66023e3cefbc0a7
Cruz Godar <cruz.godar@gmail.com>
2019-10-25 08:19:47 -0700

    Fixed bug in service worker that made it only cache image files

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,28 @@ if (workbox)
 
 
 
-//For image files, though, we'll cache them for a week. It's not as important to have the latest versions, and they are by far the biggest use of the network.
+//For non-image files, we go to the latest version on the network by default. If that's not available, then we look to the cache.
+workbox.routing.registerRoute(
+	/\.(?:html|js|css|json|xml|txt|pdf|tex|zip)$/,
+	new workbox.strategies.NetworkFirst({
+		cacheName: "main-cache",
+		plugins: [
+			new workbox.expiration.Plugin({
+				//Cache up to 1000 files.
+				maxEntries: 1000,
+				
+				//Cache for a maximum of a month. We don't really care about these going out of date, since the newest version is always fetched from the network when possible.
+				maxAgeSeconds: 30 * 24 * 60 * 60,
+				
+				purgeOnQuotaError: true
+			})
+		],
+	})
+);
+
+
+
+//For image files, though, we'll cache them for a week and serve the cached version by default. It's not as important to have the latest versions, and they are by far the biggest use of the network.
 workbox.routing.registerRoute(
 	/\.(?:webp|png|jpg|jpeg|gif)$/,
 	new workbox.strategies.CacheFirst({
@@ -19,6 +40,8 @@ workbox.routing.registerRoute(
 				
 				//Cache for a maximum of a week.
 				maxAgeSeconds: 7 * 24 * 60 * 60,
+				
+				purgeOnQuotaError: true
 			})
 		],
 	})
