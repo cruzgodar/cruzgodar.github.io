21143bd556c1774681197258b3507bd3728743d5
Cruz Godar <cruz.godar@gmail.com>
2020-03-13 15:03:17 -0700

    Stopped the service worker from caching non-image files at all

--- service-worker.js
+++ service-worker.js
@@ -7,27 +7,6 @@ if (workbox)
 
 
 
-//For non-image files, we go to the latest version on the network by default. If that's not available, then we look to the cache.
-workbox.routing.registerRoute(
-	/\.(?:html|js|css|json|xml|txt|pdf|tex|zip)$/,
-	new workbox.strategies.StaleWhileRevalidate({
-		cacheName: "main-cache",
-		plugins: [
-			new workbox.expiration.Plugin({
-				//Cache up to 1000 files.
-				maxEntries: 1000,
-				
-				//Cache for a maximum of a month. We don't really care about these going out of date, since the newest version is always fetched from the network when possible.
-				maxAgeSeconds: 30 * 24 * 60 * 60,
-				
-				purgeOnQuotaError: true
-			})
-		]
-	})
-);
-
-
-
 //For image files, though, we'll cache them for a week and serve the cached version by default. It's not as important to have the latest versions, and they are by far the biggest use of the network.
 workbox.routing.registerRoute(
 	/\.(?:webp|png|jpg|jpeg|gif)$/,
