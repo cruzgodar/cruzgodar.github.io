582f8f150632775015bc2920cd3791f9f25113e6
Cruz Godar <cruz.godar@gmail.com>
2020-04-02 12:59:25 -0700

    Updated MathJax

--- scripts/src/page-load.js
+++ scripts/src/page-load.js
@@ -507,7 +507,7 @@ function typeset_math()
 		
 		
 		
-		load_script("https://cdn.jsdelivr.net/npm/mathjax@3.0.0/es5/tex-mml-chtml.js")
+		load_script("https://cdn.jsdelivr.net/npm/mathjax@3.0.1/es5/tex-mml-chtml.js")
 		
 		.then(function()
 		{
