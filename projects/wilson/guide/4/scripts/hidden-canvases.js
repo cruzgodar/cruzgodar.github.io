3b61bedfff15e2bd08f11173bd6f71f4abab16ff
Cruz Godar <cruz.godar@gmail.com>
2021-08-08 13:34:24 -0700

    Updated Wilson guide

--- projects/wilson/guide/4/scripts/hidden-canvases.js
+++ projects/wilson/guide/4/scripts/hidden-canvases.js
@@ -75,14 +75,30 @@
 		draggables_mousemove_callback: on_drag,
 		draggables_touchmove_callback: on_drag
 	};
-
-
-
+	
+	let options_hidden =
+	{
+		renderer: "gpu",
+		
+		shader: frag_shader_source,
+		
+		canvas_width: 100,
+		canvas_height: 100
+	};
+	
+	
+	
 	let wilson = new Wilson(document.querySelector("#output-canvas"), options);
 
 	wilson.render.init_uniforms(["a", "b", "brightness_scale"]);
 
 	let draggable = wilson.draggables.add(0, 1);
+	
+	
+	
+	let wilson_hidden = new Wilson(document.querySelector("#hidden-canvas"), options_hidden);
+	
+	wilson_hidden.render.init_uniforms(["a", "b", "brightness_scale"]);
 	
 	
 	
@@ -93,15 +109,28 @@
 	
 	Page.Load.AOS.show_section(0);
 	
+	wilson.canvas.parentNode.parentNode.style.setProperty("margin-bottom", 0, "important");
+	wilson_hidden.canvas.parentNode.parentNode.style.setProperty("margin-top", 0, "important");
 	
 	
-	let resolution_input_element = document.querySelector("#resolution-input");
+	
+	let a = 0;
+	let b = 1;
 	
 	let resolution = 1000;
+	let resolution_hidden = 100;
+	
+	let last_timestamp = -1;
+	
+	
+
+	let resolution_input_element = document.querySelector("#resolution-input");
 	
 	resolution_input_element.addEventListener("input", () =>
 	{
 		resolution = parseInt(resolution_input_element.value || 1000);
+		
+		wilson.change_canvas_size(resolution, resolution);
 	});
 	
 	
@@ -116,21 +145,71 @@
 	
 	
 	//Render the inital frame.
-	wilson.gl.uniform1f(wilson.uniforms["a"], 0);
-	wilson.gl.uniform1f(wilson.uniforms["b"], 1);
-	wilson.gl.uniform1f(wilson.uniforms["brightness_scale"], 10);
-	
 	wilson.change_canvas_size(resolution, resolution);
+	window.requestAnimationFrame(draw_julia_set);
 	
-	wilson.render.draw_frame();
-
-
-
+	
+	
 	function on_drag(active_draggable, x, y, event)
 	{
-		wilson.gl.uniform1f(wilson.uniforms["a"], x);
-		wilson.gl.uniform1f(wilson.uniforms["b"], y);
-		wilson.gl.uniform1f(wilson.uniforms["brightness_scale"], 10);
+		a = x;
+		b = y;
+		
+		window.requestAnimationFrame(draw_julia_set);
+	}
+
+
+
+	function draw_julia_set(timestamp)
+	{
+		let time_elapsed = timestamp - last_timestamp;
+		
+		last_timestamp = timestamp;
+		
+		
+		
+		if (time_elapsed === 0)
+		{
+			return;
+		}
+		
+		
+		
+		wilson_hidden.gl.uniform1f(wilson_hidden.uniforms["a"], a);
+		wilson_hidden.gl.uniform1f(wilson_hidden.uniforms["b"], b);
+		
+		/*
+			Like range in a photo, the brightness will get clipped if we let it go above 1.
+			Therefore, we divide by a large number to make sure that doesn&#x2019;t happen.
+			Too large and everything will get compressed into too few dark values instead,
+			so we need to pick a brightness scale in the middle.
+		*/
+		wilson_hidden.gl.uniform1f(wilson_hidden.uniforms["brightness_scale"], 20);
+		
+		wilson_hidden.render.draw_frame();
+		
+		
+		
+		let pixel_data = wilson_hidden.render.get_pixel_data();
+		
+		let brightnesses = new Array(resolution_hidden * resolution_hidden);
+		
+		for (let i = 0; i < resolution_hidden * resolution_hidden; i++)
+		{
+			brightnesses[i] = pixel_data[4 * i] + pixel_data[4 * i + 1] + pixel_data[4 * i + 2];
+		}
+		
+		brightnesses.sort((a, b) => a - b);
+		
+		let brightness_scale = brightnesses[Math.floor(resolution_hidden * resolution_hidden * .98)] / 255 * 18;
+		
+		brightness_scale = Math.max(brightness_scale, .1);
+		
+		
+		
+		wilson.gl.uniform1f(wilson.uniforms["a"], a);
+		wilson.gl.uniform1f(wilson.uniforms["b"], b);
+		wilson.gl.uniform1f(wilson.uniforms["brightness_scale"], brightness_scale);
 		
 		wilson.render.draw_frame();
 	}
