966f33a4b14fb2e0e0da0985511d0e7dfaebbf0c
Cruz Godar <cruz.godar@gmail.com>
2021-09-21 10:15:03 -0700

    Made some double pendulum progress

--- applets/double-pendulum-fractal/scripts/double-pendulum-fractal.js
+++ applets/double-pendulum-fractal/scripts/double-pendulum-fractal.js
@@ -16,15 +16,65 @@
 	
 	
 	
+	let frag_shader_source = `
+		precision highp float;
+		
+		varying vec2 uv;
+		
+		uniform sampler2D u_texture;
+		
+		
+		
+		void main(void)
+		{
+			
+			gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+		}
+	`;
+	
+	let options_parameter_calculator =
+	{
+		renderer: "gpu",
+		
+		shader: frag_shader_source,
+		
+		canvas_width: 1000,
+		canvas_height: 1000
+	};
+	
+	let wilson_parameter_calculator = new Wilson(document.querySelector("#parameter-calculator-canvas"), options_parameter_calculator);
+	
+	
+	
+	wilson_parameter_calculator.gl.pixelStorei(wilson_parameter_calculator.gl.UNPACK_ALIGNMENT, 1);
+	wilson_parameter_calculator.gl.pixelStorei(wilson_parameter_calculator.gl.UNPACK_FLIP_Y_WEBGL, 1);
+	
+	let texture = wilson_parameter_calculator.gl.createTexture();
+	wilson_parameter_calculator.gl.bindTexture(wilson_parameter_calculator.gl.TEXTURE_2D, texture);
+	
+	//Turn off mipmapping, since in general we won't have power of two canvases.
+	wilson_parameter_calculator.gl.texParameteri(wilson_parameter_calculator.gl.TEXTURE_2D, wilson_parameter_calculator.gl.TEXTURE_WRAP_S, wilson_parameter_calculator.gl.CLAMP_TO_EDGE);
+	wilson_parameter_calculator.gl.texParameteri(wilson_parameter_calculator.gl.TEXTURE_2D, wilson_parameter_calculator.gl.TEXTURE_WRAP_T, wilson_parameter_calculator.gl.CLAMP_TO_EDGE);
+	wilson_parameter_calculator.gl.texParameteri(wilson_parameter_calculator.gl.TEXTURE_2D, wilson_parameter_calculator.gl.TEXTURE_MAG_FILTER, wilson_parameter_calculator.gl.NEAREST);
+	wilson_parameter_calculator.gl.texParameteri(wilson_parameter_calculator.gl.TEXTURE_2D, wilson_parameter_calculator.gl.TEXTURE_MIN_FILTER, wilson_parameter_calculator.gl.NEAREST);
+	
+	wilson_parameter_calculator.gl.disable(wilson_parameter_calculator.gl.DEPTH_TEST);
+	
+	wilson_parameter_calculator.gl.texImage2D(wilson_parameter_calculator.gl.TEXTURE_2D, 0, wilson_parameter_calculator.gl.RGBA, wilson_parameter_calculator.canvas_width, wilson_parameter_calculator.canvas_height, 0, wilson_parameter_calculator.gl.RGBA, wilson_parameter_calculator.gl.UNSIGNED_BYTE, image);
+	
+	
+	
 	let image_size = 2000;
 	
 	let theta_1 = 3;
-	let theta_2 = 0;
+	let theta_2 = 3;
 	let p_1 = 0;
 	let p_2 = 0;
 	
+	let max_p = 0;
+	
 	let dt = .01;
-	let steps_per_update = 3;
+	let steps_per_update = 1;
 	let gravity = 1;
 	
 	let last_timestamp = -1;
@@ -105,6 +155,20 @@
 			theta_2 += d_theta_2 * dt;
 			p_1 += d_p_1 * dt;
 			p_2 += d_p_2 * dt;
+			
+			if (Math.abs(p_1) > max_p)
+			{
+				max_p = Math.abs(p_1);
+				
+				console.log(max_p);
+			}
+			
+			if (Math.abs(p_2) > max_p)
+			{
+				max_p = Math.abs(p_2);
+				
+				console.log(max_p);
+			}
 		}
 	}
 }()
