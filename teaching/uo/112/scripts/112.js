cbde8a9f2e9882fe9200ff402f7a0be63366c893
Cruz Godar <cruz.godar@gmail.com>
2021-09-28 09:37:26 -0700

    Updated 112

--- teaching/uo/112/scripts/112.js
+++ teaching/uo/112/scripts/112.js
@@ -9,5 +9,13 @@
 		element.parentNode.setAttribute("href", "https://uoregon.zoom.us/j/5334229656");
 		
 		element.setAttribute("onclick", "Page.Navigation.redirect('https://uoregon.zoom.us/j/5334229656', true)");
+		
+		
+		
+		element = document.querySelector("[data-image-id='webwork']");
+		
+		element.parentNode.setAttribute("href", "https://webwork.uoregon.edu/webwork2/Math112-13873/");
+		
+		element.setAttribute("onclick", "Page.Navigation.redirect('https://webwork.uoregon.edu/webwork2/Math112-13873/', true)");
 	}, 1000);
 }()
