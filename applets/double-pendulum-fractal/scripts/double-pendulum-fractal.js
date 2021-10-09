98db5e640e8c8b37786a05ba7baa6623f9eaee00
Cruz Godar <cruz.godar@gmail.com>
2021-10-09 13:08:59 -0700

    Removed the web worker

--- applets/double-pendulum-fractal/scripts/double-pendulum-fractal.js
+++ applets/double-pendulum-fractal/scripts/double-pendulum-fractal.js
@@ -6,13 +6,15 @@
 	
 	let image_size = 500;
 	
+	let image = null;
+	
 	let state = null;
 	
 	let dt = .01;
 	
-	let web_worker = null;
-	
 	let drawn_fractal = false;
+	
+	let paused = false;
 	
 	
 	
@@ -28,20 +30,31 @@
 	
 	
 	
+	let resolution_input_element = document.querySelector("#resolution-input");
+	
+	let dt_input_element = document.querySelector("#dt-input");
+	
+	
+	
 	let generate_button_element = document.querySelector("#generate-button");
 	
 	generate_button_element.addEventListener("click", () =>
 	{
 		drawn_fractal = false;
 		
-		request_double_pendulum_fractal();
+		draw_double_pendulum_fractal();
 	});
 	
 	
 	
 	let hide_pendulum_drawer_canvas_button_element = document.querySelector("#hide-pendulum-drawer-canvas-button");
 	
-	hide_pendulum_drawer_canvas_button_element.addEventListener("click", hide_pendulum_drawer_canvas);
+	hide_pendulum_drawer_canvas_button_element.addEventListener("click", () =>
+	{
+		//What the actual fuck
+		hide_pendulum_drawer_canvas();
+		window.requestAnimationFrame(hide_pendulum_drawer_canvas);
+	});
 	
 	
 	
@@ -54,57 +67,133 @@
 	
 	
 	
-	function request_double_pendulum_fractal()
+	function draw_double_pendulum_fractal()
 	{
-		try {web_worker.terminate();}
-		catch(ex) {}
+		state = new Array(image_size * image_size * 2);
 		
-		if (DEBUG)
+		image = new Uint8ClampedArray(image_size * image_size * 4);
+		
+		image_size = parseInt(resolution_input_element.value || 500);
+		
+		dt = parseFloat(dt_input_element.value || .01);
+		
+		
+		
+		for (let i = 0; i < image_size / 2; i++)
 		{
-			web_worker = new Worker("/applets/double-pendulum-fractal/scripts/worker.js");
-		}
-		
-		else
-		{
-			web_worker = new Worker("/applets/double-pendulum-fractal/scripts/worker.min.js");
-		}
-		
-		Page.temporary_web_workers.push(web_worker);
-		
-		web_worker.onmessage = function(e)
-		{
-			wilson.render.draw_frame(e.data[0]);
-			
-			state = e.data[1];
-		}
-		
-		
-		
-		if (!drawn_fractal)
-		{
-			state = new Array(image_size * image_size * 2);
-			
-			
-			
-			for (let i = 0; i < image_size / 2; i++)
+			for (let j = 0; j < image_size; j++)
 			{
-				for (let j = 0; j < image_size; j++)
-				{
-					let index = 4 * (image_size * i + j);
-					
-					state[index] = (j / image_size - .5) * 2 * Math.PI;
-					state[index + 1] = (i / image_size - .5) * 2 * Math.PI;
-					state[index + 2] = 0;
-					state[index + 3] = 0;
-				}
+				let index = 4 * (image_size * i + j);
+				
+				image[index] = 0;
+				image[index + 1] = 0;
+				image[index + 2] = 0;
+				image[index + 3] = 255;
+				
+				
+				
+				state[index] = (j / image_size - .5) * 2 * Math.PI;
+				state[index + 1] = (i / image_size - .5) * 2 * Math.PI;
+				state[index + 2] = 0;
+				state[index + 3] = 0;
+				
+				
+				
+				let index_2 = 4 * (image_size * (image_size - i - 1) + (image_size - j - 1));
+				
+				image[index_2] = 0;
+				image[index_2 + 1] = 0;
+				image[index_2 + 2] = 0;
+				image[index_2 + 3] = 255;
 			}
 		}
 		
 		
 		
-		web_worker.postMessage([image_size, dt, state]);
+		window.requestAnimationFrame(draw_frame);
+		
+		
 		
 		drawn_fractal = true;
+	}
+	
+	
+	
+	function draw_frame(timestamp)
+	{
+		let time_elapsed = timestamp - last_timestamp;
+		
+		last_timestamp = timestamp;
+		
+		if (time_elapsed === 0)
+		{
+			return;
+		}
+		
+		
+		
+		for (let i = 0; i < image_size / 2; i++)
+		{
+			for (let j = 0; j < image_size; j++)
+			{
+				let index = 4 * (image_size * i + j);
+				
+				let d_theta_1 = 6 * (2 * state[index + 2] - 3 * Math.cos(state[index] - state[index + 1]) * state[index + 3]) / (16 - 9 * Math.pow(Math.cos(state[index] - state[index + 1]), 2));
+				
+				let d_theta_2 = 6 * (8 * state[index + 3] - 3 * Math.cos(state[index] - state[index + 1]) * state[index + 2]) / (16 - 9 * Math.pow(Math.cos(state[index] - state[index + 1]), 2));
+				
+				let d_p_1 = -(d_theta_1 * d_theta_2 * Math.sin(state[index] - state[index + 1]) + 3 * Math.sin(state[index])) / 2;
+				
+				let d_p_2 = (d_theta_1 * d_theta_2 * Math.sin(state[index] - state[index + 1]) - Math.sin(state[index + 1])) / 2;
+				
+				
+				
+				state[index] += d_theta_1 * dt;
+				state[index + 1] += d_theta_2 * dt;
+				state[index + 2] += d_p_1 * dt;
+				state[index + 3] += d_p_2 * dt;
+				
+				
+				
+				let x = state[index] / Math.PI;
+				let y = state[index + 1] / Math.PI;
+				
+				let p_1 = state[index + 2] / Math.PI;
+				let p_2 = state[index + 3] / Math.PI;
+				
+				
+				let hue = Math.atan2(y, x) / Math.PI + 1;
+				let saturation = Math.min((x*x + y*y) * 100, 1);
+				
+				let value_add = .9 * (1 - ((i / image_size - .5) * (i / image_size - .5) + (j / image_size - .5) * (j / image_size - .5)) * 4);
+				
+				let value = Math.min(Math.pow(p_1*p_1 + p_2*p_2, .5) + value_add, 1);
+				
+				let rgb = HSVtoRGB(hue, saturation, value);
+				
+				image[index] = rgb[0];
+				image[index + 1] = rgb[1];
+				image[index + 2] = rgb[2];
+				
+				
+				
+				let index_2 = 4 * (image_size * (image_size - i - 1) + (image_size - j - 1));
+				
+				image[index_2] = rgb[0];
+				image[index_2 + 1] = rgb[1];
+				image[index_2 + 2] = rgb[2];
+				image[index_2 + 3] = 255;
+			}
+		}
+		
+		
+		
+		wilson.render.draw_frame(image);
+		
+		if (!paused)
+		{
+			window.requestAnimationFrame(draw_frame);
+		}
 	}
 	
 	
@@ -209,7 +298,7 @@
 			return;
 		}
 		
-		web_worker.terminate();
+		paused = true;
 		
 		wilson_pendulum_drawer.canvas.style.opacity = .5;
 		
@@ -237,7 +326,9 @@
 			return;
 		}
 		
-		request_double_pendulum_fractal();
+		paused = false;
+		
+		window.requestAnimationFrame(draw_frame);
 		
 		wilson_pendulum_drawer.canvas.style.opacity = 0;
 		
