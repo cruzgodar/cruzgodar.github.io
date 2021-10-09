dd5729618dab9e1b52563de577d81a5e6a857d43
Cruz Godar <cruz.godar@gmail.com>
2021-10-08 23:02:19 -0700

    Improved the coloring algorithm

--- applets/double-pendulum-fractal/scripts/worker.js
+++ applets/double-pendulum-fractal/scripts/worker.js
@@ -18,13 +18,13 @@ let dt = .01;
 
 function draw_double_pendulum_fractal()
 {
-	let state = new Array(image_size * image_size * 4);
+	let state = new Array(image_size * image_size * 2);
 	
 	let image = new Uint8ClampedArray(image_size * image_size * 4);
 	
 	
 	
-	for (let i = 0; i < image_size; i++)
+	for (let i = 0; i < image_size / 2; i++)
 	{
 		for (let j = 0; j < image_size; j++)
 		{
@@ -39,6 +39,15 @@ function draw_double_pendulum_fractal()
 			state[index + 1] = (i / image_size - .5) * 2 * Math.PI;
 			state[index + 2] = 0;
 			state[index + 3] = 0;
+			
+			
+			
+			let index_2 = 4 * (image_size * (image_size - i - 1) + (image_size - j - 1));
+			
+			image[index_2] = 0;
+			image[index_2 + 1] = 0;
+			image[index_2 + 2] = 0;
+			image[index_2 + 3] = 255;
 		}
 	}
 	
@@ -46,13 +55,11 @@ function draw_double_pendulum_fractal()
 	
 	while (true)
 	{
-		for (let i = 0; i < image_size; i++)
+		for (let i = 0; i < image_size / 2; i++)
 		{
 			for (let j = 0; j < image_size; j++)
 			{
 				let index = 4 * (image_size * i + j);
-				
-				//gl_FragColor = vec4(normalize(vec3(abs(state.x + state.y), abs(state.x), abs(state.y)) + .05 / length(state) * vec3(1.0, 1.0, 1.0)), 1.0);
 				
 				let d_theta_1 = 6 * (2 * state[index + 2] - 3 * Math.cos(state[index] - state[index + 1]) * state[index + 3]) / (16 - 9 * Math.pow(Math.cos(state[index] - state[index + 1]), 2));
 				
@@ -74,18 +81,28 @@ function draw_double_pendulum_fractal()
 				let x = state[index] / Math.PI;
 				let y = state[index + 1] / Math.PI;
 				
-				let white_add = .05 / (Math.sqrt(x*x + y*y) + .01);
+				let p_1 = state[index + 2] / Math.PI;
+				let p_2 = state[index + 3] / Math.PI;
 				
-				let r = Math.abs(x + y) + white_add;
-				let g = Math.abs(x) + white_add;
-				let b = Math.abs(y) + white_add;
 				
-				//Normalize.
-				let magnitude = Math.sqrt(r*r + g*g + b*b);
+				let hue = Math.atan2(y, x) / Math.PI + 1;
+				let saturation = Math.min(Math.pow(x*x + y*y, .05), 1);
+				let value = Math.min(Math.pow(p_1*p_1 + p_2*p_2, .25), 1);
 				
-				image[index] = r / magnitude * 255;
-				image[index + 1] = g / magnitude * 255;
-				image[index + 2] = b / magnitude * 255;
+				let rgb = HSVtoRGB(hue, saturation, value);
+				
+				image[index] = rgb[0];
+				image[index + 1] = rgb[1];
+				image[index + 2] = rgb[2];
+				
+				
+				
+				let index_2 = 4 * (image_size * (image_size - i - 1) + (image_size - j - 1));
+				
+				image[index_2] = rgb[0];
+				image[index_2 + 1] = rgb[1];
+				image[index_2 + 2] = rgb[2];
+				image[index_2 + 3] = 255;
 			}
 		}
 		
@@ -93,4 +110,29 @@ function draw_double_pendulum_fractal()
 		
 		postMessage([image]);
 	}
+}
+
+
+
+function HSVtoRGB(h, s, v)
+{
+	let r, g, b, i, f, p, q, t;
+	
+	i = Math.floor(h * 6);
+	f = h * 6 - i;
+	p = v * (1 - s);
+	q = v * (1 - f * s);
+	t = v * (1 - (1 - f) * s);
+	
+	switch (i % 6)
+	{
+		case 0: r = v, g = t, b = p; break;
+		case 1: r = q, g = v, b = p; break;
+		case 2: r = p, g = v, b = t; break;
+		case 3: r = p, g = q, b = v; break;
+		case 4: r = t, g = p, b = v; break;
+		case 5: r = v, g = p, b = q; break;
+	}
+    
+	return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
 }
