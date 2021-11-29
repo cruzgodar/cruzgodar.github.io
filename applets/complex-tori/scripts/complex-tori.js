efc755804270e97323e47dc40104b65e3b3387f1
Cruz Godar <cruz.godar@gmail.com>
2021-11-29 14:39:51 -0800

    Implemented a broken version of an elliptic curve plotter

--- applets/complex-tori/scripts/complex-tori.js
+++ applets/complex-tori/scripts/complex-tori.js
@@ -15,11 +15,15 @@
 	
 	let wilson_wpprime = null;
 	
+	let wilson_ec_plot = null;
+	
 	
 	
 	let aspect_ratio = 1;
+	let aspect_ratio_ec_plot = 1;
 	
 	let zoom_level = 1;
+	let zoom_level_ec_plot = 1;
 	
 	let past_brightness_scales = [];
 	
@@ -27,6 +31,8 @@
 	
 	let black_point = 1;
 	let white_point = 1;
+	
+	
 	
 	let fixed_point_x = 0;
 	let fixed_point_y = 0;
@@ -39,6 +45,21 @@
 	let pan_velocity_y = 0;
 	let zoom_velocity = 0;
 	
+	
+	
+	let fixed_point_x_ec_plot = 0;
+	let fixed_point_y_ec_plot = 0;
+	
+	let next_pan_velocity_x_ec_plot = 0;
+	let next_pan_velocity_y_ec_plot = 0;
+	let next_zoom_velocity_ec_plot = 0;
+	
+	let pan_velocity_x_ec_plot = 0;
+	let pan_velocity_y_ec_plot = 0;
+	let zoom_velocity_ec_plot = 0;
+	
+	
+	
 	const pan_friction = .96;
 	const pan_velocity_start_threshhold = .0025;
 	const pan_velocity_stop_threshhold = .00025;
@@ -48,6 +69,7 @@
 	const zoom_velocity_stop_threshhold = .001;
 	
 	let last_timestamp = -1;
+	let last_timestamp_ec_plot = -1;
 	
 	let currently_animating_parameters = false;
 	let parameter_animation_frame = 0;
@@ -60,7 +82,9 @@
 	{
 		resolution = parseInt(resolution_input_element.value || 500);
 		
-		wilson.change_canvas_size(resolution, resolution);
+		wilson_wp.change_canvas_size(resolution, resolution);
+		
+		wilson_wpprime.change_canvas_size(resolution, resolution);
 		
 		window.requestAnimationFrame(draw_frame);
 	});
@@ -249,6 +273,60 @@
 		`;
 		
 		
+		
+		let frag_shader_source_ec_plot = `
+			precision highp float;
+			
+			varying vec2 uv;
+			
+			uniform float aspect_ratio;
+			
+			uniform float world_center_x;
+			uniform float world_center_y;
+			uniform float world_size;
+			
+			uniform float g2;
+			uniform float g3;
+			
+			const float threshhold = .01;
+			
+			
+			
+			float f(vec2 z)
+			{
+				return z.y * z.y   -   4.0 * z.x * z.x * z.x   +   g2 * z.x   +   g3;
+			}
+			
+			
+			
+			void main(void)
+			{
+				vec2 z;
+				
+				if (aspect_ratio >= 1.0)
+				{
+					z = vec2(uv.x * aspect_ratio * world_size + world_center_x, uv.y * world_size + world_center_y);
+				}
+				
+				else
+				{
+					z = vec2(uv.x * world_size + world_center_x, uv.y / aspect_ratio * world_size + world_center_y);
+				}
+				
+				
+				
+				if (abs(f(z)) < threshhold)
+				{
+					gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+					
+					return;
+				}
+				
+				gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+			}
+		`;
+		
+		
 
 		let options_wp =
 		{
@@ -338,6 +416,50 @@
 		
 		
 		
+		let options_ec_plot =
+		{
+			renderer: "gpu",
+			
+			shader: frag_shader_source_ec_plot,
+			
+			canvas_width: 500,
+			canvas_height: 500,
+			
+			world_width: 2,
+			world_height: 2,
+			world_center_x: 0,
+			world_center_y: 0,
+			
+			
+			
+			use_fullscreen: true,
+			
+			true_fullscreen: true,
+		
+			use_fullscreen_button: true,
+			
+			enter_fullscreen_button_icon_path: "/graphics/general-icons/enter-fullscreen.png",
+			exit_fullscreen_button_icon_path: "/graphics/general-icons/exit-fullscreen.png",
+			
+			switch_fullscreen_callback: change_aspect_ratio_ec_plot,
+			
+			
+			
+			mousedown_callback: on_grab_canvas_ec_plot,
+			touchstart_callback: on_grab_canvas_ec_plot,
+			
+			mousedrag_callback: on_drag_canvas_ec_plot,
+			touchmove_callback: on_drag_canvas_ec_plot,
+			
+			mouseup_callback: on_release_canvas_ec_plot,
+			touchend_callback: on_release_canvas_ec_plot,
+			
+			wheel_callback: on_wheel_canvas_ec_plot,
+			pinch_callback: on_pinch_canvas_ec_plot
+		};
+		
+		
+		
 		wilson_wp = new Wilson(document.querySelector("#wp-canvas"), options_wp);
 
 		wilson_wp.render.init_uniforms(["aspect_ratio", "world_center_x", "world_center_y", "world_size", "black_point", "white_point", "tau"]);
@@ -358,7 +480,19 @@
 		
 		
 		
+		wilson_ec_plot = new Wilson(document.querySelector("#ec-plot-canvas"), options_ec_plot);
+		
+		wilson_ec_plot.render.init_uniforms(["aspect_ratio", "world_center_x", "world_center_y", "world_size", "g2", "g3"]);
+		
+		wilson_ec_plot.gl.uniform1f(wilson_ec_plot.uniforms["g2"], 1);
+			
+		wilson_ec_plot.gl.uniform1f(wilson_ec_plot.uniforms["g3"], 0);
+		
+		
+		
 		window.requestAnimationFrame(draw_frame);
+		
+		window.requestAnimationFrame(draw_frame_ec_plot);
 		
 		
 		
@@ -369,6 +503,16 @@
 		pan_velocity_x = 0;
 		pan_velocity_y = 0;
 		zoom_velocity = 0;
+		
+		
+		
+		next_pan_velocity_x_ec_plot = 0;
+		next_pan_velocity_y_ec_plot = 0;
+		next_zoom_velocity_ec_plot = 0;
+		
+		pan_velocity_x_ec_plot = 0;
+		pan_velocity_y_ec_plot = 0;
+		zoom_velocity_ec_plot = 0;
 	}
 	
 	
@@ -382,6 +526,19 @@
 		next_pan_velocity_x = 0;
 		next_pan_velocity_y = 0;
 		next_zoom_velocity = 0;
+	}
+	
+	function on_grab_canvas_ec_plot(x, y, event)
+	{
+		pan_velocity_x_ec_plot = 0;
+		pan_velocity_y_ec_plot = 0;
+		zoom_velocity_ec_plot = 0;
+		
+		next_pan_velocity_x_ec_plot = 0;
+		next_pan_velocity_y_ec_plot = 0;
+		next_zoom_velocity_ec_plot = 0;
+		
+		console.log("hi!");
 	}
 	
 	
@@ -399,6 +556,17 @@
 		next_pan_velocity_y = -y_delta / wilson_wp.world_height;
 		
 		window.requestAnimationFrame(draw_frame);
+	}
+	
+	function on_drag_canvas_ec_plot(x, y, x_delta, y_delta, event)
+	{
+		wilson_ec_plot.world_center_x -= x_delta;
+		wilson_ec_plot.world_center_y -= y_delta;
+		
+		next_pan_velocity_x_ec_plot = -x_delta / wilson_ec_plot.world_width;
+		next_pan_velocity_y_ec_plot = -y_delta / wilson_ec_plot.world_height;
+		
+		window.requestAnimationFrame(draw_frame_ec_plot);
 	}
 	
 	
@@ -419,6 +587,22 @@
 		window.requestAnimationFrame(draw_frame);
 	}
 	
+	function on_release_canvas_ec_plot(x, y, event)
+	{
+		if (Math.sqrt(next_pan_velocity_x_ec_plot * next_pan_velocity_x_ec_plot + next_pan_velocity_y_ec_plot * next_pan_velocity_y_ec_plot) >= pan_velocity_start_threshhold)
+		{
+			pan_velocity_x_ec_plot = next_pan_velocity_x_ec_plot;
+			pan_velocity_y_ec_plot = next_pan_velocity_y_ec_plot;
+		}
+		
+		if (Math.abs(next_zoom_velocity_ec_plot) >= zoom_velocity_start_threshhold)
+		{
+			zoom_velocity_ec_plot = next_zoom_velocity_ec_plot;
+		}
+		
+		window.requestAnimationFrame(draw_frame_ec_plot);
+	}
+	
 	
 	
 	function on_wheel_canvas(x, y, scroll_amount, event)
@@ -437,6 +621,24 @@
 		}
 		
 		zoom_canvas();
+	}
+	
+	function on_wheel_canvas_ec_plot(x, y, scroll_amount, event)
+	{
+		fixed_point_x_ec_plot = x;
+		fixed_point_y_ec_plot = y;
+		
+		if (Math.abs(scroll_amount / 100) < .3)
+		{
+			zoom_level_ec_plot += scroll_amount / 100;
+		}
+		
+		else
+		{
+			zoom_velocity_ec_plot += Math.sign(scroll_amount) * .05;
+		}
+		
+		zoom_canvas_ec_plot();
 	}
 	
 	
@@ -461,6 +663,28 @@
 		fixed_point_y = y;
 		
 		zoom_canvas();
+	}
+	
+	function on_pinch_canvas_ec_plot(x, y, touch_distance_delta, event)
+	{
+		if (aspect_ratio_ec_plot >= 1)
+		{
+			zoom_level_ec_plot -= touch_distance_delta / wilson_ec_plot.world_width * 10;
+			
+			next_zoom_velocity_ec_plot = -touch_distance_delta / wilson_ec_plot.world_width * 10;
+		}
+		
+		else
+		{
+			zoom_level_ec_plot -= touch_distance_delta / wilson_ec_plot.world_height * 10;
+			
+			next_zoom_velocity_ec_plot = -touch_distance_delta / wilson_ec_plot.world_height * 10;
+		}
+		
+		fixed_point_x_ec_plot = x;
+		fixed_point_y_ec_plot = y;
+		
+		zoom_canvas_ec_plot();
 	}
 	
 	
@@ -488,7 +712,7 @@
 		
 		else
 		{
-			let new_world_center = wilson.input.get_zoomed_world_center(fixed_point_x, fixed_point_y, 3 * Math.pow(2, zoom_level), 3 * Math.pow(2, zoom_level) / aspect_ratio);
+			let new_world_center = wilson_wp.input.get_zoomed_world_center(fixed_point_x, fixed_point_y, 3 * Math.pow(2, zoom_level), 3 * Math.pow(2, zoom_level) / aspect_ratio);
 			
 			wilson_wp.world_width = 3 * Math.pow(2, zoom_level);
 			wilson_wp.world_height = 3 * Math.pow(2, zoom_level) / aspect_ratio;
@@ -503,6 +727,33 @@
 			
 			wilson_wpprime.world_center_x = new_world_center[0];
 			wilson_wpprime.world_center_y = new_world_center[1];
+		}
+		
+		window.requestAnimationFrame(draw_frame);
+	}
+	
+	function zoom_canvas_ec_plot()
+	{
+		if (aspect_ratio_ec_plot >= 1)
+		{
+			let new_world_center = wilson_ec_plot.input.get_zoomed_world_center(fixed_point_x_ec_plot, fixed_point_y_ec_plot, 3 * Math.pow(2, zoom_level_ec_plot) * aspect_ratio_ec_plot, 3 * Math.pow(2, zoom_level_ec_plot));
+			
+			wilson_ec_plot.world_width = 3 * Math.pow(2, zoom_level_ec_plot) * aspect_ratio_ec_plot;
+			wilson_ec_plot.world_height = 3 * Math.pow(2, zoom_level_ec_plot);
+			
+			wilson_ec_plot.world_center_x = new_world_center[0];
+			wilson_ec_plot.world_center_y = new_world_center[1];
+		}
+		
+		else
+		{
+			let new_world_center = wilson_ec_plot.input.get_zoomed_world_center(fixed_point_x_ec_plot, fixed_point_y_ec_plot, 3 * Math.pow(2, zoom_level_ec_plot), 3 * Math.pow(2, zoom_level_ec_plot) / aspect_ratio_ec_plot);
+			
+			wilson_ec_plot.world_width = 3 * Math.pow(2, zoom_level_ec_plot);
+			wilson_ec_plot.world_height = 3 * Math.pow(2, zoom_level_ec_plot) / aspect_ratio_ec_plot;
+			
+			wilson_ec_plot.world_center_x = new_world_center[0];
+			wilson_ec_plot.world_center_y = new_world_center[1];
 		}
 		
 		window.requestAnimationFrame(draw_frame);
@@ -604,6 +855,83 @@
 	
 	
 	
+	function draw_frame_ec_plot(timestamp)
+	{
+		let time_elapsed = timestamp - last_timestamp_ec_plot;
+		
+		last_timestamp_ec_plot = timestamp;
+		
+		
+		
+		if (time_elapsed === 0)
+		{
+			return;
+		}
+		
+		
+		
+		wilson_ec_plot.gl.uniform1f(wilson_ec_plot.uniforms["aspect_ratio"], aspect_ratio);
+		wilson_ec_plot.gl.uniform1f(wilson_ec_plot.uniforms["world_center_x"], wilson_wp.world_center_x);
+		wilson_ec_plot.gl.uniform1f(wilson_ec_plot.uniforms["world_center_y"], wilson_wp.world_center_y);
+		
+		wilson_ec_plot.gl.uniform1f(wilson_ec_plot.uniforms["world_size"], Math.min(wilson_ec_plot.world_height, wilson_ec_plot.world_width) / 2);
+		
+		wilson_ec_plot.render.draw_frame();
+		
+		return;
+		
+		
+		
+		if (time_elapsed >= 50)
+		{
+			pan_velocity_x_ec_plot = 0;
+			pan_velocity_y_ec_plot = 0;
+			zoom_velocity_ec_plot = 0;
+			
+			next_pan_velocity_x_ec_plot = 0;
+			next_pan_velocity_y_ec_plot = 0;
+			next_zoom_velocity_ec_plot = 0;
+		}
+		
+		
+		
+		if (pan_velocity_x_ec_plot !== 0 || pan_velocity_y_ec_plot !== 0 || zoom_velocity_ec_plot !== 0)
+		{
+			wilson_ec_plot.world_center_x += pan_velocity_x_ec_plot * wilson_ec_plot.world_width;
+			wilson_ec_plot.world_center_y += pan_velocity_y_ec_plot * wilson_ec_plot.world_height;
+			
+			
+			
+			pan_velocity_x_ec_plot *= pan_friction;
+			pan_velocity_y_ec_plot *= pan_friction;
+			
+			if (Math.sqrt(pan_velocity_x_ec_plot * pan_velocity_x_ec_plot + pan_velocity_y_ec_plot * pan_velocity_y_ec_plot) < pan_velocity_stop_threshhold)
+			{
+				pan_velocity_x_ec_plot = 0;
+				pan_velocity_y_ec_plot = 0;
+			}
+			
+			
+			
+			zoom_level_ec_plot += zoom_velocity_ec_plot;
+			
+			zoom_canvas_ec_plot(fixed_point_x_ec_plot, fixed_point_y_ec_plot);
+			
+			zoom_velocity_ec_plot *= zoom_friction_ec_plot;
+			
+			if (Math.abs(zoom_velocity_ec_plot) < zoom_velocity_stop_threshhold)
+			{
+				zoom_velocity_ec_plot = 0;
+			}
+			
+			
+			
+			window.requestAnimationFrame(draw_frame_ec_plot);
+		}
+	}
+	
+	
+	
 	function change_aspect_ratio()
 	{
 		if (wilson_wp.fullscreen.currently_fullscreen)
@@ -662,7 +990,48 @@
 		
 		window.requestAnimationFrame(draw_frame);
 	}
+	
+	function change_aspect_ratio_ec_plot()
+	{
+		if (wilson_ec_plot.fullscreen.currently_fullscreen)
+		{
+			aspect_ratio_ec_plot = window.innerWidth / window.innerHeight;
+			
+			if (aspect_ratio_ec_plot >= 1)
+			{
+				wilson_ec_plot.change_canvas_size(resolution, Math.floor(resolution / aspect_ratio_ec_plot));
+				
+				wilson_ec_plot.world_width = 3 * Math.pow(2, zoom_level_ec_plot) * aspect_ratio_ec_plot;
+				wilson_ec_plot.world_height = 3 * Math.pow(2, zoom_level_ec_plot);
+			}
+			
+			else
+			{
+				wilson_ec_plot.change_canvas_size(Math.floor(resolution * aspect_ratio_ec_plot), resolution);	
+				
+				wilson_ec_plot.world_width = 3 * Math.pow(2, zoom_level_ec_plot);
+				wilson_ec_plot.world_height = 3 * Math.pow(2, zoom_level_ec_plot) / aspect_ratio_ec_plot;
+			}
+		}
+		
+		else
+		{
+			aspect_ratio_ec_plot = 1;
+			
+			wilson_ec_plot.change_canvas_size(resolution, resolution);
+			
+			wilson_ec_plot.world_width = 3 * Math.pow(2, zoom_level_ec_plot);
+			wilson_ec_plot.world_height = 3 * Math.pow(2, zoom_level_ec_plot);
+		}
+		
+		window.requestAnimationFrame(draw_frame_ec_plot);
+	}
+	
+	
 
 	window.addEventListener("resize", change_aspect_ratio);
 	Page.temporary_handlers["resize"].push(change_aspect_ratio);
+	
+	window.addEventListener("resize", change_aspect_ratio_ec_plot);
+	Page.temporary_handlers["resize"].push(change_aspect_ratio_ec_plot);
 }()
