424eb5c96ea5d237d552d236774d2a66dd4fb697
Cruz Godar <cruz.godar@gmail.com>
2021-11-29 13:56:17 -0800

    Got synced wp and wpprime working

--- applets/complex-tori/scripts/complex-tori.js
+++ applets/complex-tori/scripts/complex-tori.js
@@ -11,7 +11,9 @@
 	
 	
 	
-	let wilson = null;
+	let wilson_wp = null;
+	
+	let wilson_wpprime = null;
 	
 	
 	
@@ -96,7 +98,7 @@
 	
 	
 	
-	use_new_code();
+	init_canvases();
 	
 	past_brightness_scales = [];
 	
@@ -104,13 +106,9 @@
 	
 	
 	
-	function use_new_code()
+	function init_canvases()
 	{
-		let generating_code = "wp(z, draggable_arg)";
-		
-		
-		
-		let frag_shader_source = `
+		let frag_shader_source_wp = `
 			precision highp float;
 			
 			varying vec2 uv;
@@ -124,7 +122,7 @@
 			uniform float black_point;
 			uniform float white_point;
 			
-			uniform vec2 draggable_arg;
+			uniform vec2 tau;
 			
 			
 			
@@ -144,7 +142,78 @@
 			//Returns f(z) for a polynomial f with given roots.
 			vec2 f(vec2 z)
 			{
-				return ${generating_code};
+				return wp(z, tau);
+			}
+			
+			
+			
+			void main(void)
+			{
+				vec2 z;
+				
+				if (aspect_ratio >= 1.0)
+				{
+					z = vec2(uv.x * aspect_ratio * world_size + world_center_x, uv.y * world_size + world_center_y);
+				}
+				
+				else
+				{
+					z = vec2(uv.x * world_size + world_center_x, uv.y / aspect_ratio * world_size + world_center_y);
+				}
+				
+				
+				
+				vec2 image_z = f(z);
+				
+				
+				
+				float modulus = length(image_z);
+				
+				float h = atan(image_z.y, image_z.x) / 6.283;
+				float s = clamp(1.0 / (1.0 + .01 * (modulus / white_point / white_point)), 0.0, 1.0);
+				float v = clamp(1.0 / (1.0 + .01 / (modulus * black_point * black_point)), 0.0, 1.0);
+				
+				gl_FragColor = vec4(hsv2rgb(vec3(h, s, v)), 1.0);
+			}
+		`;
+		
+		
+		
+		let frag_shader_source_wpprime = `
+			precision highp float;
+			
+			varying vec2 uv;
+			
+			uniform float aspect_ratio;
+			
+			uniform float world_center_x;
+			uniform float world_center_y;
+			uniform float world_size;
+			
+			uniform float black_point;
+			uniform float white_point;
+			
+			uniform vec2 tau;
+			
+			
+			
+			${COMPLEX_GLSL}
+			
+			
+			
+			vec3 hsv2rgb(vec3 c)
+			{
+				vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+				vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+				return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+			}
+			
+			
+			
+			//Returns f(z) for a polynomial f with given roots.
+			vec2 f(vec2 z)
+			{
+				return wpprime(z, tau);
 			}
 			
 			
@@ -181,11 +250,11 @@
 		
 		
 
-		let options =
+		let options_wp =
 		{
 			renderer: "gpu",
 			
-			shader: frag_shader_source,
+			shader: frag_shader_source_wp,
 			
 			canvas_width: 500,
 			canvas_height: 500,
@@ -194,13 +263,6 @@
 			world_height: 2,
 			world_center_x: 0,
 			world_center_y: 0,
-			
-			
-			
-			use_draggables: true,
-			
-			draggables_mousemove_callback: on_drag_draggable,
-			draggables_touchmove_callback: on_drag_draggable,
 			
 			
 			
@@ -232,20 +294,67 @@
 		
 		
 		
-		wilson = new Wilson(document.querySelector("#output-canvas"), options);
-
-		wilson.render.init_uniforms(["aspect_ratio", "world_center_x", "world_center_y", "world_size", "black_point", "white_point", "draggable_arg"]);
-		
-		wilson.gl.uniform1f(wilson.uniforms["aspect_ratio"], 1);
-		
-		
-		
-		if (generating_code.indexOf("draggable_arg") !== -1)
+		let options_wpprime =
 		{
-			wilson.draggables.add(.5, .866);
+			renderer: "gpu",
 			
-			wilson.gl.uniform2f(wilson.uniforms["draggable_arg"], .5, .866);
-		}
+			shader: frag_shader_source_wpprime,
+			
+			canvas_width: 500,
+			canvas_height: 500,
+			
+			world_width: 2,
+			world_height: 2,
+			world_center_x: 0,
+			world_center_y: 0,
+			
+			
+			
+			use_fullscreen: true,
+			
+			true_fullscreen: true,
+		
+			use_fullscreen_button: true,
+			
+			enter_fullscreen_button_icon_path: "/graphics/general-icons/enter-fullscreen.png",
+			exit_fullscreen_button_icon_path: "/graphics/general-icons/exit-fullscreen.png",
+			
+			switch_fullscreen_callback: change_aspect_ratio,
+			
+			
+			
+			mousedown_callback: on_grab_canvas,
+			touchstart_callback: on_grab_canvas,
+			
+			mousedrag_callback: on_drag_canvas,
+			touchmove_callback: on_drag_canvas,
+			
+			mouseup_callback: on_release_canvas,
+			touchend_callback: on_release_canvas,
+			
+			wheel_callback: on_wheel_canvas,
+			pinch_callback: on_pinch_canvas
+		};
+		
+		
+		
+		wilson_wp = new Wilson(document.querySelector("#wp-canvas"), options_wp);
+
+		wilson_wp.render.init_uniforms(["aspect_ratio", "world_center_x", "world_center_y", "world_size", "black_point", "white_point", "tau"]);
+		
+		wilson_wp.gl.uniform1f(wilson_wp.uniforms["aspect_ratio"], 1);
+			
+		wilson_wp.gl.uniform2f(wilson_wp.uniforms["tau"], .5, .866);
+		
+		
+		
+		wilson_wpprime = new Wilson(document.querySelector("#wpprime-canvas"), options_wpprime);
+
+		wilson_wpprime.render.init_uniforms(["aspect_ratio", "world_center_x", "world_center_y", "world_size", "black_point", "white_point", "tau"]);
+		
+		wilson_wpprime.gl.uniform1f(wilson_wpprime.uniforms["aspect_ratio"], 1);
+			
+		wilson_wpprime.gl.uniform2f(wilson_wpprime.uniforms["tau"], .5, .866);
 		
 		
 		
@@ -279,14 +388,15 @@
 	
 	function on_drag_canvas(x, y, x_delta, y_delta, event)
 	{
-		wilson.world_center_x -= x_delta;
-		wilson.world_center_y -= y_delta;
+		wilson_wp.world_center_x -= x_delta;
+		wilson_wp.world_center_y -= y_delta;
 		
-		next_pan_velocity_x = -x_delta / wilson.world_width;
-		next_pan_velocity_y = -y_delta / wilson.world_height;
+		wilson_wpprime.world_center_x -= x_delta;
+		wilson_wpprime.world_center_y -= y_delta;
 		
-		try {wilson.draggables.recalculate_locations();}
-		catch(ex) {}
+		//This can always use wilson_wp, since it will agree with wpprime.
+		next_pan_velocity_x = -x_delta / wilson_wp.world_width;
+		next_pan_velocity_y = -y_delta / wilson_wp.world_height;
 		
 		window.requestAnimationFrame(draw_frame);
 	}
@@ -335,16 +445,16 @@
 	{
 		if (aspect_ratio >= 1)
 		{
-			zoom_level -= touch_distance_delta / wilson.world_width * 10;
+			zoom_level -= touch_distance_delta / wilson_wp.world_width * 10;
 			
-			next_zoom_velocity = -touch_distance_delta / wilson.world_width * 10;
+			next_zoom_velocity = -touch_distance_delta / wilson_wp.world_width * 10;
 		}
 		
 		else
 		{
-			zoom_level -= touch_distance_delta / wilson.world_height * 10;
+			zoom_level -= touch_distance_delta / wilson_wp.world_height * 10;
 			
-			next_zoom_velocity = -touch_distance_delta / wilson.world_height * 10;
+			next_zoom_velocity = -touch_distance_delta / wilson_wp.world_height * 10;
 		}
 		
 		fixed_point_x = x;
@@ -359,37 +469,41 @@
 	{
 		if (aspect_ratio >= 1)
 		{
-			let new_world_center = wilson.input.get_zoomed_world_center(fixed_point_x, fixed_point_y, 3 * Math.pow(2, zoom_level) * aspect_ratio, 3 * Math.pow(2, zoom_level));
+			let new_world_center = wilson_wp.input.get_zoomed_world_center(fixed_point_x, fixed_point_y, 3 * Math.pow(2, zoom_level) * aspect_ratio, 3 * Math.pow(2, zoom_level));
 			
-			wilson.world_width = 3 * Math.pow(2, zoom_level) * aspect_ratio;
-			wilson.world_height = 3 * Math.pow(2, zoom_level);
+			wilson_wp.world_width = 3 * Math.pow(2, zoom_level) * aspect_ratio;
+			wilson_wp.world_height = 3 * Math.pow(2, zoom_level);
 			
-			wilson.world_center_x = new_world_center[0];
-			wilson.world_center_y = new_world_center[1];
+			wilson_wpprime.world_width = 3 * Math.pow(2, zoom_level) * aspect_ratio;
+			wilson_wpprime.world_height = 3 * Math.pow(2, zoom_level);
+			
+			
+			
+			wilson_wp.world_center_x = new_world_center[0];
+			wilson_wp.world_center_y = new_world_center[1];
+			
+			wilson_wpprime.world_center_x = new_world_center[0];
+			wilson_wpprime.world_center_y = new_world_center[1];
 		}
 		
 		else
 		{
 			let new_world_center = wilson.input.get_zoomed_world_center(fixed_point_x, fixed_point_y, 3 * Math.pow(2, zoom_level), 3 * Math.pow(2, zoom_level) / aspect_ratio);
 			
-			wilson.world_width = 3 * Math.pow(2, zoom_level);
-			wilson.world_height = 3 * Math.pow(2, zoom_level) / aspect_ratio;
+			wilson_wp.world_width = 3 * Math.pow(2, zoom_level);
+			wilson_wp.world_height = 3 * Math.pow(2, zoom_level) / aspect_ratio;
 			
-			wilson.world_center_x = new_world_center[0];
-			wilson.world_center_y = new_world_center[1];
+			wilson_wpprime.world_width = 3 * Math.pow(2, zoom_level);
+			wilson_wpprime.world_height = 3 * Math.pow(2, zoom_level) / aspect_ratio;
+			
+			
+			
+			wilson_wp.world_center_x = new_world_center[0];
+			wilson_wp.world_center_y = new_world_center[1];
+			
+			wilson_wpprime.world_center_x = new_world_center[0];
+			wilson_wpprime.world_center_y = new_world_center[1];
 		}
-		
-		try {wilson.draggables.recalculate_locations();}
-		catch(ex) {}
-		
-		window.requestAnimationFrame(draw_frame);
-	}
-	
-	
-	
-	function on_drag_draggable(active_draggable, x, y, event)
-	{
-		wilson.gl.uniform2f(wilson.uniforms["draggable_arg"], x, y);
 		
 		window.requestAnimationFrame(draw_frame);
 	}
@@ -411,16 +525,29 @@
 		
 		
 		
-		wilson.gl.uniform1f(wilson.uniforms["aspect_ratio"], aspect_ratio);
-		wilson.gl.uniform1f(wilson.uniforms["world_center_x"], wilson.world_center_x);
-		wilson.gl.uniform1f(wilson.uniforms["world_center_y"], wilson.world_center_y);
+		wilson_wp.gl.uniform1f(wilson_wp.uniforms["aspect_ratio"], aspect_ratio);
+		wilson_wp.gl.uniform1f(wilson_wp.uniforms["world_center_x"], wilson_wp.world_center_x);
+		wilson_wp.gl.uniform1f(wilson_wp.uniforms["world_center_y"], wilson_wp.world_center_y);
 		
-		wilson.gl.uniform1f(wilson.uniforms["world_size"], Math.min(wilson.world_height, wilson.world_width) / 2);
+		wilson_wp.gl.uniform1f(wilson_wp.uniforms["world_size"], Math.min(wilson_wp.world_height, wilson_wp.world_width) / 2);
 		
-		wilson.gl.uniform1f(wilson.uniforms["black_point"], black_point);
-		wilson.gl.uniform1f(wilson.uniforms["white_point"], white_point);
+		wilson_wp.gl.uniform1f(wilson_wp.uniforms["black_point"], black_point);
+		wilson_wp.gl.uniform1f(wilson_wp.uniforms["white_point"], white_point);
 		
-		wilson.render.draw_frame();
+		wilson_wp.render.draw_frame();
+		
+		
+		
+		wilson_wpprime.gl.uniform1f(wilson_wpprime.uniforms["aspect_ratio"], aspect_ratio);
+		wilson_wpprime.gl.uniform1f(wilson_wpprime.uniforms["world_center_x"], wilson_wpprime.world_center_x);
+		wilson_wpprime.gl.uniform1f(wilson_wpprime.uniforms["world_center_y"], wilson_wpprime.world_center_y);
+		
+		wilson_wpprime.gl.uniform1f(wilson_wpprime.uniforms["world_size"], Math.min(wilson_wpprime.world_height, wilson_wpprime.world_width) / 2);
+		
+		wilson_wpprime.gl.uniform1f(wilson_wpprime.uniforms["black_point"], black_point);
+		wilson_wpprime.gl.uniform1f(wilson_wpprime.uniforms["white_point"], white_point);
+		
+		wilson_wpprime.render.draw_frame();
 		
 		
 		
@@ -439,8 +566,11 @@
 		
 		if (pan_velocity_x !== 0 || pan_velocity_y !== 0 || zoom_velocity !== 0)
 		{
-			wilson.world_center_x += pan_velocity_x * wilson.world_width;
-			wilson.world_center_y += pan_velocity_y * wilson.world_height;
+			wilson_wp.world_center_x += pan_velocity_x * wilson_wp.world_width;
+			wilson_wp.world_center_y += pan_velocity_y * wilson_wp.world_height;
+			
+			wilson_wpprime.world_center_x += pan_velocity_x * wilson_wpprime.world_width;
+			wilson_wpprime.world_center_y += pan_velocity_y * wilson_wpprime.world_height;
 			
 			
 			
@@ -476,24 +606,38 @@
 	
 	function change_aspect_ratio()
 	{
-		if (wilson.fullscreen.currently_fullscreen)
+		if (wilson_wp.fullscreen.currently_fullscreen)
 		{
 			aspect_ratio = window.innerWidth / window.innerHeight;
 			
 			if (aspect_ratio >= 1)
 			{
-				wilson.change_canvas_size(resolution, Math.floor(resolution / aspect_ratio));
+				wilson_wp.change_canvas_size(resolution, Math.floor(resolution / aspect_ratio));
 				
-				wilson.world_width = 3 * Math.pow(2, zoom_level) * aspect_ratio;
-				wilson.world_height = 3 * Math.pow(2, zoom_level);
+				wilson_wpprime.change_canvas_size(resolution, Math.floor(resolution / aspect_ratio));
+				
+				
+				
+				wilson_wp.world_width = 3 * Math.pow(2, zoom_level) * aspect_ratio;
+				wilson_wp.world_height = 3 * Math.pow(2, zoom_level);
+				
+				wilson_wpprime.world_width = 3 * Math.pow(2, zoom_level) * aspect_ratio;
+				wilson_wpprime.world_height = 3 * Math.pow(2, zoom_level);
 			}
 			
 			else
 			{
-				wilson.change_canvas_size(Math.floor(resolution * aspect_ratio), resolution);
+				wilson_wp.change_canvas_size(Math.floor(resolution * aspect_ratio), resolution);
 				
-				wilson.world_width = 3 * Math.pow(2, zoom_level);
-				wilson.world_height = 3 * Math.pow(2, zoom_level) / aspect_ratio;
+				wilson_wpprime.change_canvas_size(Math.floor(resolution * aspect_ratio), resolution);
+				
+				
+				
+				wilson_wp.world_width = 3 * Math.pow(2, zoom_level);
+				wilson_wp.world_height = 3 * Math.pow(2, zoom_level) / aspect_ratio;
+				
+				wilson_wpprime.world_width = 3 * Math.pow(2, zoom_level);
+				wilson_wpprime.world_height = 3 * Math.pow(2, zoom_level) / aspect_ratio;
 			}
 		}
 		
@@ -501,10 +645,19 @@
 		{
 			aspect_ratio = 1;
 			
-			wilson.change_canvas_size(resolution, resolution);
 			
-			wilson.world_width = 3 * Math.pow(2, zoom_level);
-			wilson.world_height = 3 * Math.pow(2, zoom_level);
+			
+			wilson_wp.change_canvas_size(resolution, resolution);
+			
+			wilson_wpprime.change_canvas_size(resolution, resolution);
+			
+			
+			
+			wilson_wp.world_width = 3 * Math.pow(2, zoom_level);
+			wilson_wp.world_height = 3 * Math.pow(2, zoom_level);
+			
+			wilson_wpprime.world_width = 3 * Math.pow(2, zoom_level);
+			wilson_wpprime.world_height = 3 * Math.pow(2, zoom_level);
 		}
 		
 		window.requestAnimationFrame(draw_frame);
