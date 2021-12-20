7956746b40dc6a9873457024825d970802099b90
Cruz Godar <cruz.godar@gmail.com>
2021-12-19 20:11:57 -0800

    Set up 252 page

---
+++ teaching/uo/252/scripts/252.js
@@ -0,0 +1,21 @@
+!function()
+{
+	"use strict";
+	
+	setTimeout(() =>
+	{
+		let element = document.querySelector("[data-image-id='office-hours']");
+		
+		element.parentNode.setAttribute("href", "https://uoregon.zoom.us/j/5334229656");
+		
+		element.setAttribute("onclick", "Page.Navigation.redirect('https://uoregon.zoom.us/j/5334229656', true)");
+		
+		
+		
+		element = document.querySelector("[data-image-id='webwork']");
+		
+		element.parentNode.setAttribute("href", "https://webwork.uoregon.edu/webwork2/Math252-24030/");
+		
+		element.setAttribute("onclick", "Page.Navigation.redirect('https://webwork.uoregon.edu/webwork2/Math252-24030/', true)");
+	}, 1000);
+}()
