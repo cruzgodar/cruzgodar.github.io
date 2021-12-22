dc32921d2140ed77a6d3f8f8e19d954366addb29
Cruz Godar <cruz.godar@gmail.com>
2021-12-22 15:52:42 -0800

    Updated lecture notes

---
+++ teaching/uo/252/notes/2-integrals-intro/scripts/2-integrals-intro.js
@@ -0,0 +1,42 @@
+!function()
+{
+    try
+    {
+        let element = document.querySelector("#next-button");
+        
+        element.addEventListener("click", () =>
+        {
+            Page.Navigation.redirect("/teaching/uo/252/notes/3-antiderivatives/3-antiderivatives.html");
+        });
+    }
+    
+    catch(ex) {}
+    
+    
+    
+    try
+    {
+        let element = document.querySelector("#home-button");
+        
+        element.addEventListener("click", () =>
+        {
+            Page.Navigation.redirect("/teaching/uo/252/252.html");
+        });
+    }
+    
+    catch(ex) {}
+    
+    
+    
+    try
+    {
+        let element = document.querySelector("#previous-button");
+        
+        element.addEventListener("click", () =>
+        {
+            Page.Navigation.redirect("/teaching/uo/252/notes/1-riemann-sums/1-riemann-sums.html");
+        });
+    }
+    
+    catch(ex) {}
+}()
