a9991da5c3324a273f6f45cb07b85df4fc74b0cf
Cruz Godar <cruz.godar@gmail.com>
2022-01-20 14:08:45 -0800

    Updated 252

---
+++ teaching/uo/252/notes/7-exp-and-log-integrals/scripts/7-exp-and-log-integrals.js
@@ -0,0 +1,42 @@
+!function()
+{
+    try
+    {
+        let element = document.querySelector("#next-button");
+        
+        element.addEventListener("click", () =>
+        {
+            Page.Navigation.redirect("/teaching/uo/252/notes/8-inverse-trig-functions/8-inverse-trig-functions.html");
+        });
+    }
+    
+    catch(ex) {}
+    
+    
+    
+    try
+    {
+        let element = document.querySelector("#home-button");
+        
+        element.addEventListener("click", () =>
+        {
+            Page.Navigation.redirect("/teaching/uo/252/252.html");
+        });
+    }
+    
+    catch(ex) {}
+    
+    
+    
+    try
+    {
+        let element = document.querySelector("#previous-button");
+        
+        element.addEventListener("click", () =>
+        {
+            Page.Navigation.redirect("/teaching/uo/252/notes/6-u-sub/6-u-sub.html");
+        });
+    }
+    
+    catch(ex) {}
+}()
