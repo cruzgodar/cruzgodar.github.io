2d31071afe54c8732ca484796fa0a41dee5b529d
Cruz Godar <cruz.godar@gmail.com>
2022-01-27 09:39:34 -0800

    Got Pascal's triangle nearly functional

--- applets/pascals-triangle/scripts/pascals-triangle.js
+++ applets/pascals-triangle/scripts/pascals-triangle.js
@@ -5,19 +5,22 @@
 	
 	
 	let grid_size = 20;
+	let pixels_per_row = 0;
 	
 	let resolution = 3000;
 	
 	let num_colors = 3;
 	
-	let parities = new Array(grid_size);
+	let y_offset = 0;
 	
-	for (let i = 0; i < grid_size; i++)
-	{
-		parities[i] = new Array(grid_size);
-	}
+	let parities = [];
+	let coordinates = [];
+	let colors = [];
 	
-	parities[0][0] = 1;
+	let last_timestamp = -1;
+	
+	let current_row = 0;
+	let current_pixel = 0;
 	
 	
 	
@@ -48,6 +51,18 @@
 	
 	
 	
+	let triangle_size_input_element = document.querySelector("#triangle-size-input");
+	
+	triangle_size_input_element.addEventListener("keydown", (e) =>
+	{
+		if (e.keyCode === 13)
+		{
+			draw_pascals_triangle();
+		}
+	});
+	
+	
+	
 	let num_colors_input_element = document.querySelector("#num-colors-input");
 	
 	num_colors_input_element.addEventListener("keydown", (e) =>
@@ -71,25 +86,55 @@
 	
 	function draw_pascals_triangle()
 	{
+		grid_size = parseInt(triangle_size_input_element.value || 20);
+		
+		pixels_per_row = Math.round(resolution / (grid_size + 2));
+		
 		num_colors = parseInt(num_colors_input_element.value || 3);
 		
+		y_offset = (1 - Math.sqrt(3) / 2) * resolution / 2;
+		
+		current_row = 0;
+		current_pixel = 0;
 		
 		
-		wilson.ctx.fillStyle = "rgb(0, 0, 0)";
-		wilson.ctx.fillRect(0, 0, resolution, resolution);
+		
+		parities = new Array(grid_size);
+		colors = new Array(grid_size);
+		
+		for (let i = 0; i < grid_size; i++)
+		{
+			parities[i] = new Array(grid_size);
+			colors[i] = new Array(grid_size);
+		}
+		
+		parities[0][0] = 1;
+		colors[0][0] = wilson.utils.hsv_to_rgb(1 / num_colors, 1, 1);
 		
 		
 		
-		draw_hexagon(0, 0);
+		coordinates = new Array(grid_size);
+		
+		for (let i = 0; i < grid_size; i++)
+		{
+			coordinates[i] = new Array(grid_size);
+		}
+		
+		
 		
 		for (let i = 1; i < grid_size; i++)
 		{
 			parities[i][0] = 1;
 			parities[i][i] = 1;
 			
+			colors[i][0] = [...colors[0][0]];
+			colors[i][i] = [...colors[0][0]];
+			
 			for (let j = 1; j < i; j++)
 			{
 				parities[i][j] = (parities[i - 1][j - 1] + parities[i - 1][j]) % num_colors;
+				
+				colors[i][j] = wilson.utils.hsv_to_rgb(parities[i][j] / num_colors, 1, 1);
 			}
 		}
 		
@@ -99,14 +144,85 @@
 		{
 			for (let j = 0; j <= i; j++)
 			{
-				draw_hexagon(i, j);
+				coordinates[i][j] = get_coordinates(i, j);
 			}
 		}
+		
+		
+		
+		wilson.ctx.fillStyle = "rgb(0, 0, 0)";
+		wilson.ctx.fillRect(0, 0, resolution, resolution);
+		
+		wilson.ctx.lineWidth = 5;
+		
+		window.requestAnimationFrame(draw_frame);
 	}
 	
 	
 	
-	function draw_hexagon(row, col)
+	function draw_frame(timestamp)
+	{
+		let time_elapsed = timestamp - last_timestamp;
+		
+		last_timestamp = timestamp;
+		
+		if (time_elapsed === 0)
+		{
+			return;
+		}
+		
+		
+		
+		for (let i = 0; i < 10; i++)
+		{
+			let old_t = current_pixel / pixels_per_row;
+			let new_t = (current_pixel + 2) / pixels_per_row;
+			
+			
+			
+			for (let col = 0; col <= current_row; col++)
+			{
+				wilson.ctx.strokeStyle = `rgb(${colors[current_row][col][0] * (1 - old_t) + colors[current_row + 1][col][0] * old_t}, ${colors[current_row][col][1] * (1 - old_t) + colors[current_row + 1][col][1] * old_t}, ${colors[current_row][col][2] * (1 - old_t) + colors[current_row + 1][col][2] * old_t})`;
+				
+				wilson.ctx.beginPath();
+				wilson.ctx.moveTo(coordinates[current_row][col][0] * (1 - old_t) + coordinates[current_row + 1][col][0] * old_t, coordinates[current_row][col][1] * (1 - old_t) + coordinates[current_row + 1][col][1] * old_t);
+				wilson.ctx.lineTo(coordinates[current_row][col][0] * (1 - new_t) + coordinates[current_row + 1][col][0] * new_t, coordinates[current_row][col][1] * (1 - new_t) + coordinates[current_row + 1][col][1] * new_t);
+				wilson.ctx.stroke();
+				
+				
+				
+				wilson.ctx.strokeStyle = `rgb(${colors[current_row][col][0] * (1 - old_t) + colors[current_row + 1][col + 1][0] * old_t}, ${colors[current_row][col][1] * (1 - old_t) + colors[current_row + 1][col + 1][1] * old_t}, ${colors[current_row][col][2] * (1 - old_t) + colors[current_row + 1][col + 1][2] * old_t})`;
+				
+				wilson.ctx.beginPath();
+				wilson.ctx.moveTo(coordinates[current_row][col][0] * (1 - old_t) + coordinates[current_row + 1][col + 1][0] * old_t, coordinates[current_row][col][1] * (1 - old_t) + coordinates[current_row + 1][col + 1][1] * old_t);
+				wilson.ctx.lineTo(coordinates[current_row][col][0] * (1 - new_t) + coordinates[current_row + 1][col + 1][0] * new_t, coordinates[current_row][col][1] * (1 - new_t) + coordinates[current_row + 1][col + 1][1] * new_t);
+				wilson.ctx.stroke();
+			}
+			
+			
+			
+			current_pixel++;
+			
+			if (current_pixel === pixels_per_row - 1)
+			{
+				current_pixel = 0;
+				current_row++;
+			}
+			
+			if (current_row === grid_size - 1)
+			{
+				return;
+			}
+		}
+		
+		
+		
+		window.requestAnimationFrame(draw_frame);
+	}
+	
+	
+	
+	function get_coordinates(row, col)
 	{
 		let center_x = (col - Math.floor(row / 2)) * resolution / (grid_size + 2) + resolution / 2;
 		
@@ -115,9 +231,9 @@
 			center_x -= .5 * resolution / (grid_size + 2);
 		}
 		
-		let center_y = (row + 1) * Math.sqrt(3) / 2 * resolution / (grid_size + 2);
+		let center_y = (row + 1) * Math.sqrt(3) / 2 * resolution / (grid_size + 2) + y_offset;
 		
-		
+		return [center_x, center_y];
 		
 		let rgb = wilson.utils.hsv_to_rgb(parities[row][col] / num_colors, 1, 1);
 		
