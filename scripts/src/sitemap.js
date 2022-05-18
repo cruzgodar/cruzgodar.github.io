e0a4a4d9e77e5bc639de105031ba73b1a4e43d0c
Cruz Godar <cruz.godar@gmail.com>
2022-05-17 21:34:08 -0700

    Started work on a sitemap

---
+++ scripts/src/sitemap.js
@@ -0,0 +1,115 @@
+Site.page_depth =
+{
+	"/home/home.html": 0,
+	
+	
+	
+	"/gallery/gallery.html": 1,
+	"/applets/applets.html": 1,
+	"/writing/writing.html": 1,
+	"/teaching/teaching.html": 1,
+	"/about/about.html": 1,
+	
+	
+	
+	"/applets/plane-partitions/plane-partitions.html": 2,
+	"/applets/quaternionic-julia-sets/quaternionic-julia-sets.html": 2,
+	"/applets/fractal-sounds/fractal-sounds.html": 2,
+	"/applets/sorting-algorithms/sorting-algorithms.html": 2,
+	"/applets/complex-tori/complex-tori.html": 2,
+	"/applets/wordle-scorer/wordle-scorer.html": 2,
+	"/applets/domino-shuffling/domino-shuffling.html": 2,
+	"/applets/pascals-triangle-colorings/pascals-triangle-colorings.html": 2,
+	"/applets/three-body-systems/three-body-systems.html": 2,
+	"/applets/hitomezashi-patterns/hitomezashi-patterns.html": 2,
+	"/applets/double-pendulum-fractal/double-pendulum-fractal.html": 2,
+	"/applets/complex-maps/complex-maps.html": 2,
+	"/applets/lyapunov-fractals/lyapunov-fractals.html": 2,
+	"/applets/newtons-method-extended/newtons-method-extended.html": 2,
+	"/applets/the-secant-method/the-secant-method.html": 2,
+	"/applets/quasi-fuchsian-groups/quasi-fuchsian-groups.html": 2,
+	"/applets/the-mandelbulb/the-mandelbulb.html": 2,
+	"/applets/kaleidoscopic-ifs-fractals/kaleidoscopic-ifs-fractals.html": 2,
+	"/applets/binary-trees/binary-trees.html": 2,
+	"/applets/gravner-griffeath-snowflakes/gravner-griffeath-snowflakes.html": 2,
+	"/applets/finite-subdivisions/finite-subdivisions.html": 2,
+	"/applets/generalized-julia-sets/generalized-julia-sets.html": 2,
+	"/applets/strange-attractors/strange-attractors.html": 2,
+	"/applets/sudoku-generator/sudoku-generator.html": 2,
+	"/applets/julia-set-mosaic/julia-set-mosaic.html": 2,
+	"/applets/the-chaos-game/the-chaos-game.html": 2,
+	"/applets/the-kicked-rotator/the-kicked-rotator.html": 2,
+	"/applets/brownian-trees/brownian-trees.html": 2,
+	"/applets/the-barnsley-fern/the-barnsley-fern.html": 2,
+	"/applets/newtons-method/newtons-method.html": 2,
+	"/applets/calcudoku-generator/calcudoku-generator.html": 2,
+	"/applets/simulated-annealing/simulated-annealing.html": 2,
+	"/applets/abelian-sandpiles/abelian-sandpiles.html": 2,
+	"/applets/wilsons-algorithm/wilsons-algorithm.html": 2,
+	"/applets/julia-set-explorer/julia-set-explorer.html": 2,
+	
+	"/projects/wilson/wilson.html": 2,
+	
+	
+	
+	"/writing/caracore/caracore.html": 2,
+	"/writing/caligo/caligo.html": 2,
+	"/writing/desolation-point/desolation-point.html": 2,
+	"/writing/mist/mist.html": 2,
+	
+	
+	
+	"/teaching/uo/252/252.html": 2,
+	"/teaching/uo/251/251.html": 2,
+	"/teaching/uo/243/243.html": 2,
+	"/teaching/uo/112/112.html": 2,
+	"/teaching/uo/111/111.html": 2,
+	"/teaching/uo/105/105.html": 2,
+	
+	
+	
+	"/teaching/uo/252/notes/0-calc-1-review/0-calc-1-review.html": 3,
+	"/teaching/uo/252/notes/1-riemann-sums/1-riemann-sums.html": 3,
+	"/teaching/uo/252/notes/2-integrals-intro/2-integrals-intro.html": 3,
+	"/teaching/uo/252/notes/3-antiderivatives/3-antiderivatives.html": 3,
+	"/teaching/uo/252/notes/4-ftoc/4-ftoc.html": 3,
+	"/teaching/uo/252/notes/5-simple-applications/5-simple-applications.html": 3,
+	"/teaching/uo/252/notes/6-u-sub/6-u-sub.html": 3,
+	"/teaching/uo/252/notes/7-exp-and-log-integrals/7-exp-and-log-integrals.html": 3,
+	"/teaching/uo/252/notes/8-area-between-curves/8-area-between-curves.html": 3,
+	"/teaching/uo/252/notes/9-solids-of-revolution/9-solids-of-revolution.html": 3,
+	"/teaching/uo/252/notes/10-arc-length-and-surface-area/10-arc-length-and-surface-area.html": 3,
+	"/teaching/uo/252/notes/11-physical-applications/11-physical-applications.html": 3,
+	"/teaching/uo/252/notes/12-integration-by-parts/12-integration-by-parts.html": 3,
+	"/teaching/uo/252/notes/13-trig-sub/13-trig-sub.html": 3,
+	"/teaching/uo/252/notes/14-partial-fractions/14-partial-fractions.html": 3,
+	"/teaching/uo/252/notes/15-improper-integrals/15-improper-integrals.html": 3,
+	"/teaching/uo/252/notes/16-intro-to-des/16-intro-to-des.html": 3,
+	
+	
+	
+	"/teaching/uo/251/notes/0-algebra-and-trig-review/0-algebra-and-trig-review.html": 3,
+	"/teaching/uo/251/notes/1-intro-to-limits/1-intro-to-limits.html": 3,
+	"/teaching/uo/251/notes/2-limit-rules/2-limit-rules.html": 3,
+	"/teaching/uo/251/notes/3-continuity/3-continuity.html": 3,
+	"/teaching/uo/251/notes/4-intro-to-derivatives/4-intro-to-derivatives.html": 3,
+	"/teaching/uo/251/notes/5-derivative-rules/5-derivative-rules.html": 3,
+	"/teaching/uo/251/notes/6-applications-of-derivatives/6-applications-of-derivatives.html": 3,
+	"/teaching/uo/251/notes/7-exp-log-and-trig-derivatives/7-exp-log-and-trig-derivatives.html": 3,
+	"/teaching/uo/251/notes/8-the-chain-rule/8-the-chain-rule.html": 3,
+	"/teaching/uo/251/notes/9-the-inverse-function-theorem/9-the-inverse-function-theorem.html": 3,
+	"/teaching/uo/251/notes/10-implicit-differentiation/10-implicit-differentiation.html": 3,
+	"/teaching/uo/251/notes/11-optimization/11-optimization.html": 3,
+	"/teaching/uo/251/notes/12-l-hopitals-rule/12-l-hopitals-rule.html": 3,
+	
+	
+	
+	"/projects/wilson/docs/docs.html": 3,
+	
+	"/projects/wilson/guide/1/getting-started.html": 3,
+	"/projects/wilson/guide/2/draggables.html": 3,
+	"/projects/wilson/guide/3/parallelizing.html": 3,
+	"/projects/wilson/guide/4/hidden-canvases.html": 3,
+	"/projects/wilson/guide/5/fullscreen.html": 3,
+	"/projects/wilson/guide/6/interactivity.html": 3
+};
