34f103113db0309be4e49959307d679a37664e1a
Cruz Godar <cruz.godar@gmail.com>
2022-09-28 11:02:52 -0700

    Fixed the Caracore page

--- writing/caracore/scripts/caracore.js
+++ writing/caracore/scripts/caracore.js
@@ -1,4 +1,4 @@
-!async function()
+!function()
 {
 	"use strict";
 	
@@ -6,34 +6,22 @@
 	
 	Page.background_color_changed = true;
 	
-	Page.show()
+	anime({
+		targets: Site.Settings.meta_theme_color_element,
+		content: "#000000",
+		duration: 500,
+		easing: "cubicBezier(.42, 0, .58, 1)"
+	});
 	
-	.then(() =>
+	Page.element.querySelector("#flame-gradient-container").style.opacity = 1;
+	
+	setTimeout(() =>
 	{
-		anime({
-			targets: Site.Settings.meta_theme_color_element,
-			content: "#000000",
-			duration: 500,
-			easing: "cubicBezier(.42, 0, .58, 1)"
-		});
+		Page.element.querySelector("#flame-container").style.opacity = 1;
 		
-		Page.element.querySelector("#spawn-footer").previousElementSibling.remove();
-		
-		Page.element.querySelector("#hidden").style.display = "block";
-		
-		Page.element.querySelector("#flame-gradient-container").style.opacity = 1;
-		
-		setTimeout(() =>
-		{
-			Page.element.querySelector("#flame-container").style.opacity = 1;
-			
-			Page.element.querySelector("#flame-cover").style.height = 0;
-			Page.element.querySelector("#flame-gradient-space").style.height = 0;
-			
-			setTimeout(() =>
-			{
-				Page.Load.AOS.on_resize();
-			}, 1000);
-		}, 1000);
-	});			
+		Page.element.querySelector("#flame-cover").style.height = 0;
+		Page.element.querySelector("#flame-gradient-space").style.height = 0;
+	}, 1000);
+	
+	Page.show();
 }()
