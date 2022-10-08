66424a7f6d274123ed3851ef939cdb632815720f
Cruz Godar <cruz.godar@gmail.com>
2022-10-08 09:23:45 -0700

    Started getting the presentation tool up to speed

--- papers/bijectivizing-the-pt-dt-correspondence/scripts/bijectivizing-the-pt-dt-correspondence.js
+++ papers/bijectivizing-the-pt-dt-correspondence/scripts/bijectivizing-the-pt-dt-correspondence.js
@@ -3,14 +3,4 @@
 	Page.Presentation.init();
 	
 	Page.show();
-	
-	setTimeout(() =>
-	{
-		Page.Animate.fade_up_out(Page.element, Site.page_animation_time)
-		
-		.then(() =>
-		{
-			Page.Animate.fade_up_in(Page.element, Site.page_animation_time * 2)
-		});
-	}, 1000);
 }()
--- scripts/src/presentations.js
+++ scripts/src/presentations.js
@@ -3,17 +3,135 @@ Page.Presentation =
 	callbacks: {},
 	
 	slides: [],
+	slide_container: null,
+	
+	current_slide: -1,
+	
+	
 	
 	init: function(callbacks)
 	{
 		this.callbacks = callbacks;
 		
 		this.slides = Page.element.querySelectorAll(".slide");
+		this.slide_container = Page.element.querySelector("#slide-container");
 		
-		Page.element.querySelectorAll("header, footer").forEach(element => element.remove());
+		this.slides.forEach(element => element.style.display = "none");
 		
-		Page.element.firstElementChild.remove();
+		Page.element.querySelectorAll("header, footer").forEach(element => element.style.display = "none");
+		
+		Page.element.firstElementChild.style.display = "none";
 		
 		document.documentElement.style.overflowY = "hidden";
+		
+		
+		
+		document.documentElement.addEventListener("keydown", this.handle_keydown_event);
+		Page.temporary_handlers["keydown"].push(this.handle_keydown_event);
+		
+		
+		
+		this.next_slide();
+	},
+	
+	
+	
+	exit: function()
+	{
+		this.slide_container.remove();
+		
+		this.slides.forEach(element => element.remove());
+		
+		Page.element.querySelectorAll("header, footer").forEach(element => element.style.display = "block");
+	
+		Page.element.firstElementChild.style.display = "block";
+		
+		document.documentElement.style.overflowY = "visible";
+	},
+	
+	
+	
+	next_slide: async function()
+	{
+		if (this.current_slide === this.slides.length)
+		{
+			return;
+		}
+		
+		await Page.Animate.fade_up_out(Page.element, Site.page_animation_time);
+		
+		
+		
+		if (this.current_slide !== -1)
+		{
+			this.slides[this.current_slide].style.display = "none";
+			
+			document.body.appendChild(this.slides[this.current_slide]);
+		}	
+		
+		this.current_slide++;
+		
+		if (this.current_slide === this.slides.length)
+		{
+			this.exit();
+		}
+		
+		else
+		{
+			this.slides[this.current_slide].style.display = "block";
+			
+			this.slide_container.appendChild(this.slides[this.current_slide]);
+		}
+		
+		try {this.callbacks[this.current_slide]()}
+		catch(ex) {}
+		
+		
+		
+		Page.Animate.fade_up_in(Page.element, Site.page_animation_time * 2);
+	},
+	
+	
+	
+	previous_slide: async function()
+	{
+		if (this.current_slide === 0 || this.current_slide === this.slides.length)
+		{
+			return;
+		}
+		
+		
+		
+		await Page.Animate.fade_down_out(Page.element, Site.page_animation_time);
+		
+		this.slides[this.current_slide].style.display = "none";
+		
+		document.body.appendChild(this.slides[this.current_slide]);
+		
+		this.current_slide--;
+		
+		this.slides[this.current_slide].style.display = "block";
+			
+		this.slide_container.appendChild(this.slides[this.current_slide]);
+		
+		try {this.callbacks[this.current_slide]()}
+		catch(ex) {}
+		
+		Page.Animate.fade_down_in(Page.element, Site.page_animation_time * 2);
+	},
+	
+	
+	
+	handle_keydown_event: function(e)
+	{
+		if (e.keyCode === 39 || e.keyCode === 40 || e.keyCode === 32 || e.keyCode === 13)
+		{
+			Page.Presentation.next_slide();
+		}
+		
+		else if (e.keyCode === 37 || e.keyCode === 38)
+		{
+			Page.Presentation.previous_slide();
+		}
 	}
 }
