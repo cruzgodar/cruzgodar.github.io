b666ef440e08bd750246b71737dda429a859f81d
Cruz Godar <cruz.godar@gmail.com>
2022-10-14 14:49:58 -0700

    Fixed presentation resizing

--- papers/bijectivizing-the-pt-dt-correspondence/scripts/bijectivizing-the-pt-dt-correspondence.js
+++ papers/bijectivizing-the-pt-dt-correspondence/scripts/bijectivizing-the-pt-dt-correspondence.js
@@ -13,4 +13,28 @@
 	Page.Presentation.init(callbacks);
 	
 	Page.show();
+	
+	
+	
+	let options =
+	{
+		renderer: "cpu",
+		
+		canvas_width: 1000,
+		canvas_height: 1000,
+		
+		
+		
+		use_fullscreen: true,
+	
+		use_fullscreen_button: true,
+		
+		enter_fullscreen_button_icon_path: "/graphics/general-icons/enter-fullscreen.png",
+		exit_fullscreen_button_icon_path: "/graphics/general-icons/exit-fullscreen.png"
+	};
+	
+	let wilson = new Wilson(Page.element.querySelector("#output-canvas"), options);
+	
+	wilson.ctx.fillStyle = "rgb(0, 0, 0)";
+	wilson.ctx.fillRect(0, 0, 1000, 1000);
 }()
