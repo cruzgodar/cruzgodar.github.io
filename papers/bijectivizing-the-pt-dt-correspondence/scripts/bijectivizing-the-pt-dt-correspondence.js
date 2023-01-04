78390ff22c6de9895136555eb4c9844ea9559fb0
Cruz Godar <cruz.godar@gmail.com>
2023-01-04 13:54:05 -0800

    Added Lapsa to the site

--- papers/bijectivizing-the-pt-dt-correspondence/scripts/bijectivizing-the-pt-dt-correspondence.js
+++ papers/bijectivizing-the-pt-dt-correspondence/scripts/bijectivizing-the-pt-dt-correspondence.js
@@ -4,6 +4,8 @@
 	
 	
 	
+	Page.element.querySelectorAll("#slide-container > br").forEach(element => element.remove());
+	
 	//This thing is constructed in a really, *really* strange way in order to decrease storing effectively duplicate files. This script fetches the plane partition applet's code and a collection of presentation-specific code that's in this directory, merges them into a new function, and then executes that. This ensures everything has the correct scope and that the two scripts can interact without needing to store and maintain the entire plane partitions script in here.
 	
 	let min_extension = DEBUG ? "" : "min.";
