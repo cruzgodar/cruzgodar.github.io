d8dcc1bc9c18e076094e5014bcb5ebe00ff6e5df
Cruz Godar <cruz.godar@gmail.com>
2023-02-17 11:46:01 -0800

    Updated 251

---
+++ teaching/uo/251/notes/13-related-rates/scripts/13-related-rates.js
@@ -0,0 +1,25 @@
+!function()
+{
+	Page.Load.get_desmos_data = () =>
+	{
+		const data =
+		{
+			"related-rates":
+			{
+				bounds: {left: -10, right: 110, bottom: -10, top: 110},
+				
+				expressions:
+				[
+					{latex: String.raw`(0, 0), (10t, 100), (10t, 0), (0, 0)`, color: DESMOS_PURPLE, lines: true},
+					{latex: String.raw`t = 10`},
+				]
+			},
+		};
+		
+		return data;
+	}
+	
+	Page.Load.create_desmos_graphs();
+	
+	Page.show();
+}()
