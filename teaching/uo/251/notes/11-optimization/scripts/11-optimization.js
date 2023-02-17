2b8066999028849b9dbae7eb42f3d357cb0430af
Cruz Godar <cruz.godar@gmail.com>
2023-02-17 11:06:02 -0800

    Updated 251

---
+++ teaching/uo/251/notes/11-optimization/scripts/11-optimization.js
@@ -0,0 +1,81 @@
+!function()
+{
+	Page.Load.get_desmos_data = () =>
+	{
+		const data =
+		{
+			"extrema":
+			{
+				bounds: {left: -3.5, right: 3.5, bottom: -3.5, top: 3.5},
+				
+				expressions:
+				[
+					{latex: String.raw`f(x) = x^5 - 3x^3 + x \{-1.5 \leq x \leq 1.5\}`, color: DESMOS_PURPLE, secret: true},
+					{latex: String.raw`(-3, 0), (-2, 2), (-1.5, f(-1.5))`, secret: true, points: false, lines: true, color: DESMOS_PURPLE},
+					{latex: String.raw`(-3, 0), (-1.5, f(-1.5)), (-.345, f(-.345)), (1.297, f(1.297))`, secret: true, color: DESMOS_BLUE},
+					{latex: String.raw`(-2, 2), (-1.297, f(-1.297)), (.345, f(.345)), (1.5, f(1.5))`, secret: true, color: DESMOS_RED},
+				]
+			},
+			
+			
+			
+			"saddle-point":
+			{
+				bounds: {left: -3, right: 3, bottom: -3, top: 3},
+				
+				expressions:
+				[
+					{latex: String.raw`f(x) = x^3`, color: DESMOS_PURPLE},
+					{latex: String.raw`(0, 0)`, color: DESMOS_BLUE},
+				]
+			},
+			
+			
+			
+			"critical-points":
+			{
+				bounds: {left: -2.5, right: 2.5, bottom: -15, top: 10},
+				
+				expressions:
+				[
+					{latex: String.raw`f(x) = x^5 - 3x^3 + x \left\{-2 \leq x \leq 1.5\right\}`, color: DESMOS_PURPLE},
+				]
+			},
+			
+			
+			
+			"concavity":
+			{
+				bounds: {left: -2.5, right: 2.5, bottom: -2.5, top: 2.5},
+				
+				expressions:
+				[
+					{latex: String.raw`f(x) = x^5 - 3x^3 + x`, color: DESMOS_PURPLE, hidden: true, secret: true},
+					{latex: String.raw`f(x)`, color: DESMOS_PURPLE},
+					{latex: String.raw`f''(x)`, color: DESMOS_GREEN, hidden: true},
+					
+					{latex: String.raw`y \geq f(x) \{-.949 \leq x \leq 0, .949 \leq x\}`, color: DESMOS_RED, secret: true},
+					{latex: String.raw`y \leq f(x) \{x \leq -.949, 0 \leq x \leq .949\}`, color: DESMOS_BLUE, secret: true},
+				]
+			},
+			
+			
+			
+			"first-derivative-test":
+			{
+				bounds: {left: -2.5, right: 2.5, bottom: -2.5, top: 2.5},
+				
+				expressions:
+				[
+					{latex: String.raw`g(t) = t^6 - t^4`, color: DESMOS_PURPLE},
+				]
+			}
+		};
+		
+		return data;
+	}
+	
+	Page.Load.create_desmos_graphs();
+	
+	Page.show();
+}()
