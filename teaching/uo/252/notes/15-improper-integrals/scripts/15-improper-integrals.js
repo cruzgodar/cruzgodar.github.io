b21c087b791dbe518d1aaf066933a9dc9366bba3
Cruz Godar <cruz.godar@gmail.com>
2023-02-21 09:54:01 -0800

    Updated 252

---
+++ teaching/uo/252/notes/15-improper-integrals/scripts/15-improper-integrals.js
@@ -0,0 +1,90 @@
+!function()
+{
+	Page.Load.get_desmos_data = () =>
+	{
+		const data =
+		{
+			"improper-integral":
+			{
+				bounds: {left: -5, right: 5, bottom: -5, top: 5},
+				
+				expressions:
+				[
+					{latex: String.raw`f(x) = \frac{1}{x^2}`, color: DESMOS_PURPLE},
+					{latex: String.raw`a = 1`},
+					{latex: String.raw`b = \infty`},
+					
+					{latex: String.raw`x = [a, b] \{0 \leq y \leq f(x)\}`, color: DESMOS_PURPLE, secret: true},
+					{latex: String.raw`x = [a, b] \{f(x) \leq y \leq 0\}`, color: DESMOS_PURPLE, secret: true},
+					{latex: String.raw`0 \leq y \leq f(x) \{a \leq x \leq b\}`, color: DESMOS_PURPLE, secret: true},
+					{latex: String.raw`f(x) \leq y \leq 0 \{a \leq x \leq b\}`, color: DESMOS_PURPLE, secret: true},
+				]
+			},
+			
+			
+			
+			"improper-integral-2":
+			{
+				bounds: {left: -5, right: 5, bottom: -5, top: 5},
+				
+				expressions:
+				[
+					{latex: String.raw`f(x) = \frac{1}{x}`, color: DESMOS_PURPLE},
+					{latex: String.raw`a = -\infty`},
+					{latex: String.raw`b = -1`},
+					
+					{latex: String.raw`x = [a, b] \{0 \leq y \leq f(x)\}`, color: DESMOS_PURPLE, secret: true},
+					{latex: String.raw`x = [a, b] \{f(x) \leq y \leq 0\}`, color: DESMOS_PURPLE, secret: true},
+					{latex: String.raw`0 \leq y \leq f(x) \{a \leq x \leq b\}`, color: DESMOS_PURPLE, secret: true},
+					{latex: String.raw`f(x) \leq y \leq 0 \{a \leq x \leq b\}`, color: DESMOS_PURPLE, secret: true},
+				]
+			},
+			
+			
+			
+			"improper-integral-3":
+			{
+				bounds: {left: -5, right: 5, bottom: -5, top: 5},
+				
+				expressions:
+				[
+					{latex: String.raw`f(x) = \frac{1}{\sqrt{4 - x}}`, color: DESMOS_PURPLE},
+					{latex: String.raw`a = 0`},
+					{latex: String.raw`b = 4`},
+					
+					{latex: String.raw`x = [a, b] \{0 \leq y \leq f(x)\}`, color: DESMOS_PURPLE, secret: true},
+					{latex: String.raw`x = [a, b] \{f(x) \leq y \leq 0\}`, color: DESMOS_PURPLE, secret: true},
+					{latex: String.raw`0 \leq y \leq f(x) \{a \leq x \leq b\}`, color: DESMOS_PURPLE, secret: true},
+					{latex: String.raw`f(x) \leq y \leq 0 \{a \leq x \leq b\}`, color: DESMOS_PURPLE, secret: true},
+				]
+			},
+			
+			
+			
+			"improper-integral-4":
+			{
+				bounds: {left: -4, right: 4, bottom: -4, top: 4},
+				
+				expressions:
+				[
+					{latex: String.raw`f(x) = \frac{1}{x^3}`, color: DESMOS_PURPLE},
+					{latex: String.raw`a = -1`},
+					{latex: String.raw`b = 1`},
+					
+					{latex: String.raw`x = [a, b] \{0 \leq y \leq f(x)\}`, color: DESMOS_PURPLE, secret: true},
+					{latex: String.raw`x = [a, b] \{f(x) \leq y \leq 0\}`, color: DESMOS_PURPLE, secret: true},
+					{latex: String.raw`0 \leq y \leq f(x) \{a \leq x \leq b\}`, color: DESMOS_PURPLE, secret: true},
+					{latex: String.raw`f(x) \leq y \leq 0 \{a \leq x \leq b\}`, color: DESMOS_PURPLE, secret: true},
+				]
+			},
+		};
+		
+		return data;
+	};
+	
+	Page.Load.create_desmos_graphs();
+	
+	
+	
+	Page.show();
+}()
---
+++ teaching/uo/252/notes/16-intro-to-des/scripts/16-intro-to-des.js
@@ -0,0 +1,88 @@
+!function()
+{
+	Page.Load.get_desmos_data = () =>
+	{
+		const data =
+		{
+			"direction-field":
+			{
+				bounds: {left: -10, right: 10, bottom: -10, top: 10},
+				
+				expressions:
+				[
+					{latex: String.raw`f(x, y) = \frac{1}{30}xy`},
+					{latex: String.raw`n = 10`, sliderBounds: {min: 1, max: 20, step: 1}},
+					{latex: String.raw`I = [0, 1, ..., (2n + 1)^2 - 1]`, secret: true},
+					{latex: String.raw`A = [-n, -n + 1, ..., n]`, secret: true},
+					{latex: String.raw`X = A[\mod(I, 2n + 1) + 1]`, secret: true},
+					{latex: String.raw`Y = X[\floor(I / (2n + 1)) + 1]`, secret: true},
+					{latex: String.raw`f(X, Y)(x - X) + Y \{\left|x - X\right| \leq \frac{.3}{\sqrt{1 + f(X, Y)^2}}\}`, color: DESMOS_PURPLE, secret: true},
+				]
+			},
+			
+			
+			
+			"direction-field-2":
+			{
+				bounds: {left: -10, right: 10, bottom: -10, top: 10},
+				
+				expressions:
+				[
+					{latex: String.raw`f(x, y) = \frac{1}{20}(x^2 - 1)\sin(y)`},
+					{latex: String.raw`n = 10`, sliderBounds: {min: 1, max: 20, step: 1}},
+					{latex: String.raw`I = [0, 1, ..., (2n + 1)^2 - 1]`, secret: true},
+					{latex: String.raw`A = [-n, -n + 1, ..., n]`, secret: true},
+					{latex: String.raw`X = A[\mod(I, 2n + 1) + 1]`, secret: true},
+					{latex: String.raw`Y = X[\floor(I / (2n + 1)) + 1]`, secret: true},
+					{latex: String.raw`f(X, Y)(x - X) + Y \{\left|x - X\right| \leq \frac{.3}{\sqrt{1 + f(X, Y)^2}}\}`, color: DESMOS_PURPLE, secret: true},
+				]
+			},
+			
+			
+			
+			"de-solution":
+			{
+				bounds: {left: -10, right: 10, bottom: -10, top: 10},
+				
+				expressions:
+				[
+					{latex: String.raw`f(x, y) = xy`},
+					{latex: String.raw`c = .1`, sliderBounds: {min: -1, max: 1}},
+					{latex: String.raw`y = ce^{x^2}`, color: DESMOS_BLUE},
+					{latex: String.raw`n = 10`, sliderBounds: {min: 1, max: 20, step: 1}},
+					{latex: String.raw`I = [0, 1, ..., (2n + 1)^2 - 1]`, secret: true},
+					{latex: String.raw`A = [-n, -n + 1, ..., n]`, secret: true},
+					{latex: String.raw`X = A[\mod(I, 2n + 1) + 1]`, secret: true},
+					{latex: String.raw`Y = X[\floor(I / (2n + 1)) + 1]`, secret: true},
+					{latex: String.raw`f(X, Y)(x - X) + Y \{\left|x - X\right| \leq \frac{.3}{\sqrt{1 + f(X, Y)^2}}\}`, color: DESMOS_PURPLE, secret: true},
+				]
+			},
+			
+			
+			
+			"direction-field-3":
+			{
+				bounds: {left: -10, right: 10, bottom: -10, top: 10},
+				
+				expressions:
+				[
+					{latex: String.raw`f(x, y) = \frac{1}{20}(x - 3)(y^2 - 4)`},
+					{latex: String.raw`n = 10`, sliderBounds: {min: 1, max: 20, step: 1}},
+					{latex: String.raw`I = [0, 1, ..., (2n + 1)^2 - 1]`, secret: true},
+					{latex: String.raw`A = [-n, -n + 1, ..., n]`, secret: true},
+					{latex: String.raw`X = A[\mod(I, 2n + 1) + 1]`, secret: true},
+					{latex: String.raw`Y = X[\floor(I / (2n + 1)) + 1]`, secret: true},
+					{latex: String.raw`f(X, Y)(x - X) + Y \{\left|x - X\right| \leq \frac{.3}{\sqrt{1 + f(X, Y)^2}}\}`, color: DESMOS_PURPLE, secret: true},
+				]
+			},
+		};
+		
+		return data;
+	};
+	
+	Page.Load.create_desmos_graphs();
+	
+	
+	
+	Page.show();
+}()
