ac6ffbf6fa2192747303aa48fcf7a0cdf0598e2a
Cruz Godar <cruz.godar@gmail.com>
2023-03-08 23:47:08 -0800

    Updated presentation a lil

--- slides/oral-exam/scripts/index.js
+++ slides/oral-exam/scripts/index.js
@@ -14,11 +14,11 @@
 	
 	//This thing is constructed in a really, *really* strange way in order to decrease storing effectively duplicate files. This script fetches the plane partition applet's code and a collection of presentation-specific code that's in this directory, merges them into a new function, and then executes that. This ensures everything has the correct scope and that the two scripts can interact without needing to store and maintain the entire plane partitions script in here.
 	
-	let applet_response = await fetch(`/applets/plane-partitions/scripts/content.js`);
+	let applet_response = await fetch(`/applets/plane-partitions/scripts/content.${DEBUG ? "min." : ""}js`);
 	
 	let applet_data = await applet_response.text();
 	
-	let presentation_response = await fetch(`/slides/oral-exam/scripts/content.js`);
+	let presentation_response = await fetch(`/slides/oral-exam/scripts/content.${DEBUG ? "min." : ""}js`);
 	
 	let presentation_data = await presentation_response.text();
 	
