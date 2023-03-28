f76bc29b3321c30b6e4dac451e814698fe4fb847
Cruz Godar <cruz.godar@gmail.com>
2023-03-28 10:27:27 -0700

    Updated 256

--- scripts/src/sitemap.js
+++ scripts/src/sitemap.js
@@ -413,6 +413,7 @@ Site.sitemap =
 			"/teaching/uo/256/notes/0-calc-review/",
 			"/teaching/uo/256/notes/1-intro-to-des/",
 			"/teaching/uo/256/notes/2-if-and-sep/",
+			"/teaching/uo/256/notes/3-simple-applications/",
 		]
 	},
 		
@@ -431,6 +432,13 @@ Site.sitemap =
 	},
 	
 	"/teaching/uo/256/notes/2-if-and-sep/":
+	{
+		parent: "/teaching/uo/256/",
+		
+		children: []
+	},
+	
+	"/teaching/uo/256/notes/3-simple-applications/":
 	{
 		parent: "/teaching/uo/256/",
 		
