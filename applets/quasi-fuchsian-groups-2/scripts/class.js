1414f9727746568f9238008ffc7c7d21105008f4
Cruz Godar <cruz.godar@gmail.com>
2023-04-11 14:13:18 -0700

    Made some more progress

--- applets/quasi-fuchsian-groups-2/scripts/class.js
+++ applets/quasi-fuchsian-groups-2/scripts/class.js
@@ -4,7 +4,13 @@ class QuasiFuchsianGroup extends Applet
 {
 	resolution = 500;
 	
-	coefficients = [[[1,0],[0,0],[0,-2],[1,0]],[[1,-1],[1,0],[1,0],[1,1]],[[1,0],[0,0],[0,2],[1,0]],[[1,1],[-1,0],[-1,0],[1,-1]]];
+	computations_per_frame = 25;
+	
+	max_brightness = 50;
+	max_depth = 10;
+	epsilon = .01;
+	
+	particles = [];
 	
 	last_timestamp = -1;
 	
@@ -24,7 +30,7 @@ class QuasiFuchsianGroup extends Applet
 		super(canvas);
 		
 		this.update_canvas = document.createElement("canvas");
-		this.update_canvas.classList.add("output-canvas");
+		this.update_canvas.classList.add("hidden-canvas");
 		this.hidden_canvases.push(this.update_canvas);
 		Page.element.appendChild(this.update_canvas);
 		
@@ -37,7 +43,8 @@ class QuasiFuchsianGroup extends Applet
 			
 			varying vec2 uv;
 			
-			uniform sampler2D u_texture;
+			uniform vec2 start_z;
+			uniform int last_m;
 			
 			const float resolution = 243.0;
 			
@@ -148,14 +155,10 @@ class QuasiFuchsianGroup extends Applet
 			
 			void main(void)
 			{
-				vec2 location = (uv + vec2(1.0, 1.0)) / 2.0;
+				vec2 location = (uv + vec2(1.0, 1.0)) / 2.0 * resolution;
 				
-				vec4 state = texture2D(u_texture, location);
-				
-				vec2 z = state.xy;
-				int m = int(state.z);
-				
-				location *= resolution;
+				vec2 z = start_z;
+				int m = last_m;
 				
 				int m_string[10];
 				
@@ -247,7 +250,7 @@ class QuasiFuchsianGroup extends Applet
 			${update_shader_base}
 				}
 				
-				gl_FragColor = vec4(float(m) / 4.0, 0.0, 0.0, 1.0);
+				gl_FragColor = vec4(float(m) / 4.0, 0.0, 0.0, 0.0);
 			}
 		`;
 		
@@ -264,8 +267,11 @@ class QuasiFuchsianGroup extends Applet
 		this.wilson_update = new Wilson(this.update_canvas, options_update);
 		
 		this.wilson_update.render.load_new_shader(update_shader_y);
-		
 		this.wilson_update.render.load_new_shader(update_shader_m);
+		
+		this.wilson_update.render.init_uniforms(["start_z", "last_m"], 0);
+		this.wilson_update.render.init_uniforms(["start_z", "last_m"], 1);
+		this.wilson_update.render.init_uniforms(["start_z", "last_m"], 2);
 		
 		
 		
@@ -276,6 +282,8 @@ class QuasiFuchsianGroup extends Applet
 			varying vec2 uv;
 			
 			uniform sampler2D u_texture;
+			
+			const float max_brightness = 50.0;
 			
 			
 			
@@ -290,7 +298,7 @@ class QuasiFuchsianGroup extends Applet
 			{
 				float state = texture2D(u_texture, (uv + vec2(1.0, 1.0)) / 2.0).x;
 				
-				gl_FragColor = vec4(hsv2rgb(vec3(atan(uv.y, uv.x) / 6.28318530718, 1.0, state)), 1.0);
+				gl_FragColor = vec4(hsv2rgb(vec3(atan(uv.y, uv.x) / 6.28318530718, 1.0, state)) * 255.0 / max_brightness, 1.0);
 			}
 		`;
 		
@@ -321,9 +329,11 @@ class QuasiFuchsianGroup extends Applet
 	
 	
 	
-	run(resolution = 500)
+	run(resolution = 500, computations_per_frame = 10)
 	{
 		this.resolution = resolution;
+		
+		this.computations_per_frame = computations_per_frame;
 		
 		this.wilson.change_canvas_size(this.resolution, this.resolution);
 		
@@ -340,6 +350,8 @@ class QuasiFuchsianGroup extends Applet
 			this.draw_texture[index + 2] = 0;
 			this.draw_texture[index + 3] = 255;
 		}
+		
+		this.particles = [[[0, 0], 0, 0]];
 		
 		
 		
@@ -361,45 +373,94 @@ class QuasiFuchsianGroup extends Applet
 		
 		
 		
-		this.wilson_update.gl.useProgram(this.wilson_update.render.shader_programs[0]);
-		
-		this.wilson_update.render.draw_frame();
-		
-		const floats_x = new Float32Array(this.wilson_update.render.get_pixel_data().buffer);
-		
-		
-		
-		this.wilson_update.gl.useProgram(this.wilson_update.render.shader_programs[1]);
-		
-		this.wilson_update.render.draw_frame();
-		
-		const floats_y = new Float32Array(this.wilson_update.render.get_pixel_data().buffer);
-		
-		
-		
-		this.wilson_update.gl.useProgram(this.wilson_update.render.shader_programs[2]);
-		
-		this.wilson_update.render.draw_frame();
-		
-		const last_m = this.wilson_update.render.get_pixel_data();
-		
-		
-		
-		for (let i = 0; i < floats_x.length; i++)
+		for (let iteration = 0; iteration < this.computations_per_frame; iteration++)
 		{
-			const coordinates = this.wilson.utils.interpolate.world_to_canvas(floats_x[i], floats_y[i]);
+			const particle_index = Math.floor(Math.random() * this.particles.length);
 			
-			if (coordinates[0] >= 0 && coordinates[0] < this.resolution && coordinates[1] >= 0 && coordinates[1] < this.resolution)
+			
+			
+			this.wilson_update.gl.useProgram(this.wilson_update.render.shader_programs[0]);
+			
+			this.wilson_update.gl.uniform2fv(this.wilson_update.uniforms["start_z"][0], this.particles[particle_index][0]);
+			this.wilson_update.gl.uniform1i(this.wilson_update.uniforms["last_m"][0], this.particles[particle_index][1]);
+			
+			this.wilson_update.render.draw_frame();
+			
+			const floats_x = new Float32Array(this.wilson_update.render.get_pixel_data().buffer);
+			
+			
+			
+			this.wilson_update.gl.useProgram(this.wilson_update.render.shader_programs[1]);
+			
+			this.wilson_update.gl.uniform2fv(this.wilson_update.uniforms["start_z"][1], this.particles[particle_index][0]);
+			this.wilson_update.gl.uniform1i(this.wilson_update.uniforms["last_m"][1], this.particles[particle_index][1]);
+			
+			this.wilson_update.render.draw_frame();
+			
+			const floats_y = new Float32Array(this.wilson_update.render.get_pixel_data().buffer);
+			
+			
+			
+			this.wilson_update.gl.useProgram(this.wilson_update.render.shader_programs[2]);
+			
+			this.wilson_update.gl.uniform2fv(this.wilson_update.uniforms["start_z"][2], this.particles[particle_index][0]);
+			this.wilson_update.gl.uniform1i(this.wilson_update.uniforms["last_m"][2], this.particles[particle_index][1]);
+			
+			this.wilson_update.render.draw_frame();
+			
+			const last_m = this.wilson_update.render.get_pixel_data();
+			
+			
+			
+			for (let i = 0; i < floats_x.length; i++)
 			{
-				const index = 4 * (this.resolution * coordinates[0] + coordinates[1]);
+				const coordinates = this.wilson.utils.interpolate.world_to_canvas(floats_x[i], floats_y[i]);
 				
-				this.draw_texture[index]++;
+				if (coordinates[0] >= 0 && coordinates[0] < this.resolution && coordinates[1] >= 0 && coordinates[1] < this.resolution)
+				{
+					const index = 4 * (this.resolution * coordinates[0] + coordinates[1]);
+					
+					if (this.draw_texture[index] < this.max_brightness)
+					{
+						if (this.particles[particle_index][2] >= 2)
+						{
+							this.draw_texture[index]++;
+						}
+						
+						//Add this point to the queue for further processing.
+						if (this.particles[particle_index][2] < this.max_depth)
+						{
+							//Make sure this particle isn't already in the queue.
+							let found_match = false;
+							
+							for (let j = 0; j < this.particles.length; j++)
+							{
+								const dx = this.particles[j][0][0] - floats_x[i];
+								const dy = this.particles[j][0][1] - floats_y[i];
+								
+								if (dx * dx + dy * dy < this.epsilon * this.epsilon)
+								{
+									found_match = true;
+									break;
+								}
+							}
+							
+							if (!found_match)
+							{
+								this.particles.push([[floats_x[i], floats_y[i]], last_m[i], this.particles[particle_index][2] + 1]);
+							}
+						}
+					}
+				}
 			}
+			
+			
+			this.particles.splice(particle_index, 1);
 		}
 		
 		
 		
-		this.wilson.gl.texImage2D(this.wilson.gl.TEXTURE_2D, 0, this.wilson.gl.RGBA, this.wilson.canvas_width, this.wilson.canvas_height, 0, this.wilson.gl.RGBA, this.wilson.gl.UNSIGNED_BYTE, this.draw_texture);
+		this.wilson.gl.texImage2D(this.wilson.gl.TEXTURE_2D, 0, this.wilson.gl.RGBA, this.resolution, this.resolution, 0, this.wilson.gl.RGBA, this.wilson.gl.UNSIGNED_BYTE, this.draw_texture);
 		
 		this.wilson.render.draw_frame();
 		
@@ -407,7 +468,7 @@ class QuasiFuchsianGroup extends Applet
 		
 		if (!this.animation_paused)
 		{
-			//window.requestAnimationFrame(this.draw_frame.bind(this));
+			window.requestAnimationFrame(this.draw_frame.bind(this));
 		}
 	}
 	
