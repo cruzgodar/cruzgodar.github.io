4cfb917cacf872fd874ee3fde58bd7c1d97afaf4
Cruz Godar <cruz.godar@gmail.com>
2023-07-01 10:57:17 -0700

    Added minification to the build script

--- build.sh
+++ build.sh
@@ -1,1 +1,14 @@
-node ./build/build.mjs
+clean=''
+
+while getopts 'c' flag; do
+  case "${flag}" in
+    c) clean='true'
+  esac
+done
+
+if [[ $clean == "true" ]]
+then
+	node ./build/build.mjs -c
+else
+	node ./build/build.mjs
+fi
--- build/build.mjs
+++ build/build.mjs
@@ -8,7 +8,7 @@ const root = process.argv[1].replace(/(\/90259025.github.io\/).+$/, (match, $1) 
 
 const excludeFromBuild = [/build\/[^\/]+\./];
 
-
+const clean = process.argv.slice(2).includes("-c");
 
 async function buildSite()
 {
@@ -17,7 +17,7 @@ async function buildSite()
 	const text = await read(sitemapPath);
 	const sitemap = JSON.parse(text.slice(text.indexOf("{")));
 
-	exec(`git -C ${root} ls-files -m -o`, (error, stdout, stderr) =>
+	exec(`git -C ${root} ls-files${clean ? "" : " -m -o"}`, (error, stdout, stderr) =>
 	{
 		parseModifiedFiles(stdout.split("\n"), sitemap);
 	});
@@ -51,7 +51,31 @@ function parseModifiedFiles(files, sitemap)
 		{
 			buildHTMLFile(await read(file), "/" + file.slice(0, lastSlashIndex), sitemap);
 		}
+
+		else if (extension === "mjs" || extension === "js")
+		{
+			buildJSFile(file);
+		}
+
+		else if (extension === "css")
+		{
+			buildCSSFile(file);
+		}
 	});
+}
+
+function buildJSFile(file)
+{
+	const outputFile = file.replace(/(\.m*js)/, (match, $1) => `.min${$1}`);
+
+	exec(`google-closure-compiler --js ${file} --js_output_file ${outputFile} --language_in UNSTABLE --language_out UNSTABLE`);
+}
+
+function buildCSSFile(file)
+{
+	const outputFile = file.replace(/(\.css)/, (match, $1) => `.min${$1}`);
+
+	exec(`uglifycss ${file} --output ${outputFile}`);
 }
 
 
