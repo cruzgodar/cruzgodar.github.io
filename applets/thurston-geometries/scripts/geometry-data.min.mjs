3429a0d6b4736e962dfd28d6e7c74f29e3f3faf0
Cruz Godar <cruz.godar@gmail.com>
2023-09-18 16:44:15 -0700

    Reorganized the data a little

--- applets/thurston-geometries/scripts/geometry-data.min.mjs
+++ applets/thurston-geometries/scripts/geometry-data.min.mjs
@@ -1,4 +1,4 @@
-import{ThurstonGeometry}from"./class.min.mjs";const updateCameraPosFunctions={S3:(cameraPos,tangentVec,dt)=>{for(let s=0;s<4;s++)cameraPos[s]=Math.cos(dt)*cameraPos[s]+Math.sin(dt)*tangentVec[s];var a=ThurstonGeometry.magnitude(cameraPos);cameraPos[0]/=a,cameraPos[1]/=a,cameraPos[2]/=a,cameraPos[3]/=a}},getNormalVecFunctions={S3:cameraPos=>ThurstonGeometry.normalize(cameraPos)},getGammaPrimeFunctions={S3:(_pos,dir)=>[...dir]},getGammaDoublePrimeFunctions={S3:pos=>[-pos[0],-pos[1],-pos[2],-pos[3]]};function getS3SpheresData(){return{updateCameraPos:updateCameraPosFunctions.S3,getNormalVec:getNormalVecFunctions.S3,getGammaPrime:getGammaPrimeFunctions.S3,getGammaDoublePrime:getGammaDoublePrimeFunctions.S3,distanceEstimatorGlsl:`
+import{ThurstonGeometry}from"./class.min.mjs";function getS3BaseData(){return{updateCameraPos:(cameraPos,tangentVec,dt)=>{for(let s=0;s<4;s++)cameraPos[s]=Math.cos(dt)*cameraPos[s]+Math.sin(dt)*tangentVec[s];var a=ThurstonGeometry.magnitude(cameraPos);cameraPos[0]/=a,cameraPos[1]/=a,cameraPos[2]/=a,cameraPos[3]/=a},getNormalVec:cameraPos=>ThurstonGeometry.normalize(cameraPos),getGammaPrime:(_pos,dir)=>[...dir],getGammaDoublePrime:pos=>[-pos[0],-pos[1],-pos[2],-pos[3]],cameraPos:[0,0,0,-1],normalVec:[0,0,0,-1],upVec:[0,0,1,0],rightVec:[0,1,0,0],forwardVec:[1,0,0,0]}}function getS3SpheresData(){return{...getS3BaseData(),distanceEstimatorGlsl:`
 			float distance1 = acos(pos.x) - .3;
 			float distance2 = acos(-pos.x) - .3;
 			float distance3 = acos(pos.y) - .3;
@@ -73,7 +73,7 @@ import{ThurstonGeometry}from"./class.min.mjs";const updateCameraPosFunctions={S3
 			{
 				return vec3(1.0, 1.0, 1.0) * getBanding(pos.x + pos.y + pos.z, 10.0);
 			}
-		`,cameraPos:[0,0,0,-1],normalVec:[0,0,0,-1],upVec:[0,0,1,0],rightVec:[0,1,0,0],forwardVec:[1,0,0,0]}}function getS3RoomsData(){return{updateCameraPos:updateCameraPosFunctions.S3,getNormalVec:getNormalVecFunctions.S3,getGammaPrime:getGammaPrimeFunctions.S3,getGammaDoublePrime:getGammaDoublePrimeFunctions.S3,distanceEstimatorGlsl:`
+		`}}function getS3RoomsData(){return{...getS3BaseData(),distanceEstimatorGlsl:`
 			float distance1 = acos(pos.x) - .9;
 			float distance2 = acos(-pos.x) - .9;
 			float distance3 = acos(pos.y) - .9;
@@ -155,4 +155,4 @@ import{ThurstonGeometry}from"./class.min.mjs";const updateCameraPosFunctions={S3
 			{
 				return vec3(.5, 0.0, 1.0);
 			}
-		`,cameraPos:[0,0,0,-1],normalVec:[0,0,0,-1],upVec:[0,0,1,0],rightVec:[0,1,0,0],forwardVec:[1,0,0,0]}}export{getS3SpheresData,getS3RoomsData};
+		`}}export{getS3SpheresData,getS3RoomsData};
--- applets/thurston-geometries/scripts/geometry-data.mjs
+++ applets/thurston-geometries/scripts/geometry-data.mjs
@@ -1,68 +1,64 @@
 import { ThurstonGeometry } from "./class.mjs";
 
-const updateCameraPosFunctions = {
-	S3: (cameraPos, tangentVec, dt) =>
-	{
-		for (let i = 0; i < 4; i++)
+function getS3BaseData()
+{
+	return {
+		updateCameraPos: (cameraPos, tangentVec, dt) =>
 		{
-			//This will change with the geometry.
-			cameraPos[i] =
-				Math.cos(dt) * cameraPos[i]
-				+ Math.sin(dt) * tangentVec[i];
-		}
-		
-		//Since we're only doing a linear approximation, this position won't be exactly
-		//on the manifold. Therefore, we'll do a quick correction to get it back.
+			for (let i = 0; i < 4; i++)
+			{
+				//This will change with the geometry.
+				cameraPos[i] =
+					Math.cos(dt) * cameraPos[i]
+					+ Math.sin(dt) * tangentVec[i];
+			}
+			
+			//Since we're only doing a linear approximation, this position won't be exactly
+			//on the manifold. Therefore, we'll do a quick correction to get it back.
 
-		//Here, we just want the magnitude to be equal to 1
-		const magnitude = ThurstonGeometry.magnitude(cameraPos);
+			//Here, we just want the magnitude to be equal to 1
+			const magnitude = ThurstonGeometry.magnitude(cameraPos);
 
-		cameraPos[0] /= magnitude;
-		cameraPos[1] /= magnitude;
-		cameraPos[2] /= magnitude;
-		cameraPos[3] /= magnitude;
-	}
-};
+			cameraPos[0] /= magnitude;
+			cameraPos[1] /= magnitude;
+			cameraPos[2] /= magnitude;
+			cameraPos[3] /= magnitude;
+		},
 
+		getNormalVec: (cameraPos) =>
+		{
+			//f = x^2 + y^2 + z^2 + w^2 - 1.
+			return ThurstonGeometry.normalize(cameraPos);
+		},
 
+		getGammaPrime: (_pos, dir) =>
+		{
+			//gamma = cos(t)*pos + sin(t)*dir
+			//gamma' = -sin(t)*pos + cos(t)*dir
+			//gamma'' = -cos(t)*pos - sin(t)*dir
+			//All of these are evaluated at t=0.
+			return [...dir];
+		},
 
-const getNormalVecFunctions = {
-	S3: (cameraPos) =>
-	{
-		//f = x^2 + y^2 + z^2 + w^2 - 1.
-		return ThurstonGeometry.normalize(cameraPos);
-	}
-};
+		getGammaDoublePrime: (pos) =>
+		{
+			return [-pos[0], -pos[1], -pos[2], -pos[3]];
+		},
 
-
-
-const getGammaPrimeFunctions = {
-	S3: (_pos, dir) =>
-	{
-		//gamma = cos(t)*pos + sin(t)*dir
-		//gamma' = -sin(t)*pos + cos(t)*dir
-		//gamma'' = -cos(t)*pos - sin(t)*dir
-		//All of these are evaluated at t=0.
-		return [...dir];
-	}
-};
-
-const getGammaDoublePrimeFunctions = {
-	S3: (pos) =>
-	{
-		return [-pos[0], -pos[1], -pos[2], -pos[3]];
-	}
-};
+		cameraPos: [0, 0, 0, -1],
+		normalVec: [0, 0, 0, -1],
+		upVec: [0, 0, 1, 0],
+		rightVec: [0, 1, 0, 0],
+		forwardVec: [1, 0, 0, 0],
+	};
+}
 
 
 
 export function getS3SpheresData()
 {
 	return {
-		updateCameraPos: updateCameraPosFunctions.S3,
-		getNormalVec: getNormalVecFunctions.S3,
-		getGammaPrime: getGammaPrimeFunctions.S3,
-		getGammaDoublePrime: getGammaDoublePrimeFunctions.S3,
+		...getS3BaseData(),
 
 		distanceEstimatorGlsl: `
 			float distance1 = acos(pos.x) - .3;
@@ -142,12 +138,6 @@ export function getS3SpheresData()
 				return vec3(1.0, 1.0, 1.0) * getBanding(pos.x + pos.y + pos.z, 10.0);
 			}
 		`,
-
-		cameraPos: [0, 0, 0, -1],
-		normalVec: [0, 0, 0, -1],
-		upVec: [0, 0, 1, 0],
-		rightVec: [0, 1, 0, 0],
-		forwardVec: [1, 0, 0, 0],
 	};
 }
 
@@ -156,10 +146,7 @@ export function getS3SpheresData()
 export function getS3RoomsData()
 {
 	return {
-		updateCameraPos: updateCameraPosFunctions.S3,
-		getNormalVec: getNormalVecFunctions.S3,
-		getGammaPrime: getGammaPrimeFunctions.S3,
-		getGammaDoublePrime: getGammaDoublePrimeFunctions.S3,
+		...getS3BaseData(),
 
 		distanceEstimatorGlsl: `
 			float distance1 = acos(pos.x) - .9;
@@ -246,11 +233,5 @@ export function getS3RoomsData()
 				return vec3(.5, 0.0, 1.0);
 			}
 		`,
-
-		cameraPos: [0, 0, 0, -1],
-		normalVec: [0, 0, 0, -1],
-		upVec: [0, 0, 1, 0],
-		rightVec: [0, 1, 0, 0],
-		forwardVec: [1, 0, 0, 0],
 	};
 }
