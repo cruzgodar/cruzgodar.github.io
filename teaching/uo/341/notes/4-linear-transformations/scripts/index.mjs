1c775f6807dd5941613230c230c16fade5daeac1
Cruz Godar <cruz.godar@gmail.com>
2023-10-09 13:58:39 -0700

    Updated 341

--- teaching/uo/341/notes/4-linear-transformations/scripts/index.mjs
+++ teaching/uo/341/notes/4-linear-transformations/scripts/index.mjs
@@ -1,6 +1,8 @@
 import {
 	createDesmosGraphs,
+	desmosBlue,
 	desmosPurple,
+	desmosRed,
 	getDesmosPoint,
 	getDesmosSlider,
 	getDesmosVector,
@@ -20,10 +22,51 @@ export function load()
 
 				expressions:
 				[
-					...getDesmosSlider({ expression: "a = 1" }),
-					...getDesmosSlider({ expression: "b = 0" }),
-					...getDesmosPoint({ point: ["a", "b"], color: desmosPurple, dragMode: "X" }),
-					...getDesmosVector({ from: [0, 0], to: ["a", "b"], color: desmosPurple })
+					...getDesmosSlider({ expression: "a = 2", secret: false }),
+					...getDesmosPoint({ point: ["a", 0], dragMode: "X", color: desmosRed }),
+					...getDesmosVector({ from: [0, 0], to: ["a", "0"], color: desmosBlue }),
+					...getDesmosVector({ from: [0, 0], to: [1, 0], color: desmosPurple }),
+				]
+			},
+
+			"2d-linear-transformation":
+			{
+				bounds: { left: -5, right: 5, bottom: -5, top: 5 },
+
+				expressions:
+				[
+					...getDesmosSlider({ expression: "a_{11} = 0", secret: false }),
+					...getDesmosSlider({ expression: "a_{12} = 1", secret: false }),
+					...getDesmosSlider({ expression: "a_{21} = -2", secret: false }),
+					...getDesmosSlider({ expression: "a_{22} = 1", secret: false }),
+					...getDesmosSlider({ expression: "b = 2", secret: false }),
+					...getDesmosSlider({ expression: "c = 1", secret: false }),
+					
+					...getDesmosPoint({ point: ["a_{11}", "a_{21}"], color: desmosRed }),
+					...getDesmosPoint({ point: ["a_{12}", "a_{22}"], color: desmosBlue }),
+					...getDesmosPoint({ point: ["b", "c"], color: desmosPurple }),
+
+					...getDesmosVector({ from: [0, 0], to: [1, 0], color: desmosRed }),
+					...getDesmosVector({ from: [0, 0], to: [0, 1], color: desmosBlue }),
+					...getDesmosVector({ from: [0, 0], to: ["b", "c"], color: desmosPurple }),
+
+					...getDesmosVector({
+						from: [0, 0],
+						to: ["a_{11}", "a_{21}"],
+						color: desmosRed
+					}),
+
+					...getDesmosVector({
+						from: [0, 0],
+						to: ["a_{12}", "a_{22}"],
+						color: desmosBlue
+					}),
+
+					...getDesmosVector({
+						from: [0, 0],
+						to: ["a_{11}b + a_{12}c", "a_{21}b + a_{22}c"],
+						color: desmosPurple
+					}),
 				]
 			},
 		};
