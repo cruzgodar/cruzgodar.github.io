ec36a3c675e5d6d283746a644c0974acf4ae7ba8
Cruz Godar <cruz.godar@gmail.com>
2024-03-21 15:29:40 -0700

    Reverted homepage changes

--- home/scripts/index.js
+++ home/scripts/index.js
@@ -1,59 +1,68 @@
-import { showPage } from "../../scripts/src/loadPage.js";
-import { fadeUpIn } from "/scripts/src/animation.js";
-import { $ } from "/scripts/src/main.js";
+import { disableLinks, showPage } from "../../scripts/src/loadPage.js";
+import { fadeLeft, opacityAnimationTime } from "/scripts/src/animation.js";
+import { scrollButtonOpacity } from "/scripts/src/banners.js";
+import {
+	$,
+	addTemporaryListener,
+	setVisitedHomepage,
+	visitedHomepage
+} from "/scripts/src/main.js";
+
+// On large screens, make the content be centered at the bottom of the page.
+function centerContent()
+{
+	const contentHeight = $("main").getBoundingClientRect().height;
+	const marginBottom = Math.max(128, (window.innerHeight - contentHeight) / 2);
+	$("section:last-of-type").style.marginBottom = `${marginBottom}px`;
+}
 
 export function load()
 {
+	if (visitedHomepage)
+	{
+		$("#return-scroll-to").scrollIntoView();
+	}
+
+	setVisitedHomepage(true);
+
+	addTemporaryListener({
+		object: window,
+		event: "resize",
+		callback: centerContent
+	});
+
+	centerContent();
+
+
+
 	const cruzTextElement = $("#cruz-text");
 	const godarTextElement = $("#godar-text");
 
-	setTimeout(() => fadeUpIn({ element: cruzTextElement }), 500);
+	function onScroll()
+	{
+		window.requestAnimationFrame(() =>
+		{
+			cruzTextElement.parentNode.style.opacity = scrollButtonOpacity;
+			godarTextElement.parentNode.style.opacity = scrollButtonOpacity;
+		});
+	}
 
-	// if (visitedHomepage)
-	// {
-	// 	$("#return-scroll-to").scrollIntoView();
-	// }
+	setTimeout(() =>
+	{
+		fadeLeft(cruzTextElement);
 
-	// setVisitedHomepage(true);
+		setTimeout(() => fadeLeft(godarTextElement), opacityAnimationTime);
+	}, opacityAnimationTime);
 
-	// addTemporaryListener({
-	// 	object: window,
-	// 	event: "resize",
-	// 	callback: centerContent
-	// });
+	setTimeout(() => onScroll(1), 100);
 
-	// centerContent();
+	addTemporaryListener({
+		object: window,
+		event: "scroll",
+		callback: onScroll
+	});
 
-
-
-	// const cruzTextElement = $("#cruz-text");
-	// const godarTextElement = $("#godar-text");
-
-	// function onScroll()
-	// {
-	// 	window.requestAnimationFrame(() =>
-	// 	{
-	// 		cruzTextElement.parentNode.style.opacity = scrollButtonOpacity;
-	// 		godarTextElement.parentNode.style.opacity = scrollButtonOpacity;
-	// 	});
-	// }
-
-	// setTimeout(() =>
-	// {
-	// 	fadeLeft(cruzTextElement);
-
-	// 	setTimeout(() => fadeLeft(godarTextElement), opacityAnimationTime);
-	// }, opacityAnimationTime);
-
-	// setTimeout(() => onScroll(1), 100);
-
-	// addTemporaryListener({
-	// 	object: window,
-	// 	event: "scroll",
-	// 	callback: onScroll
-	// });
-
-	// disableLinks();
+	disableLinks();
 
 	showPage();
 }
