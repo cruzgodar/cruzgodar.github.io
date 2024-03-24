102b0d302613fe0782a395e4d034508d4fe399d4
Cruz Godar <cruz.godar@gmail.com>
2024-03-24 10:30:55 -0700

    Updated 342

--- teaching/uo/342/notes/4-the-dot-product/scripts/index.js
+++ teaching/uo/342/notes/4-the-dot-product/scripts/index.js
@@ -1,6 +1,7 @@
 import {
 	createDesmosGraphs,
 	desmosBlue,
+	desmosGreen,
 	desmosPurple,
 	desmosRed,
 	getDesmosVector,
@@ -23,6 +24,34 @@ export function load()
 					...getDesmosVector({ from: [0, 0], to: [3, 2], color: desmosPurple }),
 					...getDesmosVector({ from: [0, 0], to: [-2, 1], color: desmosBlue }),
 					...getDesmosVector({ from: [-2, 1], to: [3, 2], color: desmosRed })
+				]
+			},
+
+			"gram-schmidt":
+			{
+				bounds: { left: -5, right: 5, bottom: -5, top: 5 },
+
+				expressions:
+				[
+					...getDesmosVector({ from: [0, 0], to: [3, 2], color: desmosPurple }),
+					...getDesmosVector({ from: [0, 0], to: [-3, 1], color: desmosBlue }),
+
+					...getDesmosVector({
+						from: [0, 0],
+						to: ["\\frac{-21}{13}", "\\frac{-14}{13}"],
+						color: desmosRed
+					}),
+
+					...getDesmosVector({
+						from: ["\\frac{-21}{13}", "\\frac{-14}{13}"],
+						to: [-3, 1],
+						color: desmosGreen
+					}),
+					...getDesmosVector({
+						from: [0, 0],
+						to: ["-3 - \\frac{-21}{13}", "1 - \\frac{-14}{13}"],
+						color: desmosGreen
+					}),
 				]
 			},
 		};
