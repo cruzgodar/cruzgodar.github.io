31ffd1f450bdd90478a806d2eaecad8e7859b71d
Cruz Godar <cruz.godar@gmail.com>
2024-10-16 10:13:13 -0700

    Did a lot of gallery cleaning

--- build/buildGallery.js
+++ build/buildGallery.js
@@ -1,5 +1,6 @@
 import { spawn, spawnSync } from "child_process";
-import { read } from "./file-io.js";
+import { getModifiedDate, read } from "./file-io.js";
+import { galleryImageData } from "/gallery/scripts/imageData.js";
 
 const root = process.argv[1].replace(/(\/cruzgodar.github.io\/).+$/, (match, $1) => $1);
 
@@ -49,6 +50,58 @@ async function makeGalleryImage(file)
 	console.log(filename);
 }
 
+async function testImageData(files)
+{
+	for (const key of files)
+	{
+		const file = key.slice(0, key.lastIndexOf("."));
+
+		if (!(file in galleryImageData))
+		{
+			console.error(`${file} is not in imageData.js`);
+			continue;
+		}
+
+		const imageData = galleryImageData[file];
+
+		if (!imageData.title)
+		{
+			console.error(`${file} has no title`);
+		}
+
+		if (!imageData.appletLink)
+		{
+			console.error(`${file} has no appletLink`);
+		}
+
+		if (!imageData.driveId)
+		{
+			console.error(`${file} has no driveId`);
+		}
+
+		const applet = await getModifiedDate(`applets/${imageData.appletLink}/index.htmdl`);
+		if (!applet)
+		{
+			console.error(`${file} has an invalid applet link`);
+		}
+	}
+
+	for (const key of Object.keys(galleryImageData))
+	{
+		const thumbnail = await getModifiedDate(`gallery/thumbnails/${key}.webp`);
+		if (!thumbnail)
+		{
+			console.error(`${key} has no thumbnail`);
+		}
+
+		const highRes = await getModifiedDate(`gallery/high-res/${key}.webp`);
+		if (!highRes)
+		{
+			console.error(`${key} has no high-res`);
+		}
+	}
+}
+
 export async function buildGallery()
 {
 	const proc = spawnSync("ls", [], { cwd: `${root}gallery/full-res/` });
@@ -59,7 +112,7 @@ export async function buildGallery()
 		files.map(file => makeGalleryImage(`gallery/full-res/${file}`))
 	);
 
-	console.log("\nDone!");
+	await testImageData(files);
 }
 
 buildGallery();
--- gallery/scripts/imageData.js
+++ gallery/scripts/imageData.js
@@ -1,5 +1,4 @@
 /* eslint-disable quotes */
- 
 
 const bridges2022 = /* html */`the <a href='https://gallery.bridgesmathart.org/exhibitions/2022-bridges-conference'>2022 Bridges conference</a>`;
 const bridges2023 = /* html */`the <a href='https://gallery.bridgesmathart.org/exhibitions/2023-bridges-conference'>2023 Bridges conference</a>`;
@@ -11,111 +10,6 @@ const girlsAngle = /* html */`the <a href='https://www.girlsangle.org/page/bulle
 
 export const galleryImageData =
 {
-	"voronoi-diagram-1":
-	{
-		title: "A Voronoi Diagram",
-
-		appletLink: "voronoi-diagrams",
-
-		driveId: "1J1dfHbqx1tX1qkkuJF0ZUp-OPlRezEsV"
-	},
-
-	"voronoi-diagram-2":
-	{
-		title: "A Voronoi Diagram",
-
-		appletLink: "voronoi-diagrams",
-
-		driveId: "1TrBXxbjYeHMbznxnT1NiNXWSj8jVBthA"
-	},
-
-	"thurston-geometry-h3":
-	{
-		title: "The Thurston Geometry $\\mathbb{H}^3$",
-
-		appletLink: "thurston-geometries",
-
-		driveId: "1wm59bChDwRgm91lONQR1bVKTCBKGWien"
-	},
-
-	"thurston-geometry-e3":
-	{
-		title: "The Thurston Geometry $\\mathbb{E}^3$",
-
-		appletLink: "thurston-geometries",
-
-		driveId: "1XqXEVoe82btwYcTolWTKT3tlsWz_98c0"
-	},
-
-	"thurston-geometry-nil":
-	{
-		title: "The Thurston Geometry Nil",
-
-		appletLink: "thurston-geometries",
-
-		driveId: "1-1h44Q9CEVGqyCkkxAQ-ykODsVzWYKwE"
-	},
-	
-	"thurston-geometry-sl2r":
-	{
-		title: "The Thurston Geometry $\\widetilde{\\operatorname{SL}}(2, \\mathbb{R})$",
-
-		appletLink: "thurston-geometries",
-
-		driveId: "1XBJVX2kWVWz1dH06DIAVlDeqr21ei8Ty"
-	},
-
-	"thurston-geometry-sol":
-	{
-		title: "The Thurston Geometry Sol",
-
-		appletLink: "thurston-geometries",
-
-		driveId: "1Pz0IUsM43_-qEXIf27CWzjOkTnMyLXz6"
-	},
-
-	"thurston-geometry-s2xe":
-	{
-		title: "The Thurston Geometry $S^2 \\times \\mathbb{E}$",
-
-		appletLink: "thurston-geometries",
-
-		driveId: "1bA57Lhq12oJ1W611UkRjOwuTs8mIXtm-"
-	},
-
-	"thurston-geometry-h2xe":
-	{
-		title: "The Thurston Geometry $\\mathbb{H}^2 \\times \\mathbb{E}$",
-
-		appletLink: "thurston-geometries",
-
-		driveId: "1LCBMWW4n8IcF3_bGo4up4dcO0ISDqH0n"
-	},
-	
-	"thurston-geometry-s3":
-	{
-		title: "The Thurston Geometry $S^3$",
-
-		appletLink: "thurston-geometries",
-
-		driveId: "1yvG5KfAg7qiYjzBDuXDilaSV23X-KyzB"
-	},
-
-	"newtons-method-2":
-	{
-		title: "Newton&#x2019;s Method",
-
-		parameters: /* html */`
-			Roots at $(\\pm 1, 0), (0, \\pm 1.5), (\\pm 1.5, \\pm 1.5)$
-		`,
-
-		featured: `A prior version of this image was featured in ${emu}.`,
-
-		appletLink: "newtons-method",
-
-		driveId: "1T8X5JduKEg8BkakXzGXZzu3CFW_R88i6"
-	},
-
 	"abelian-sandpile":
 	{
 		title: "An Abelian Sandpile",
@@ -136,44 +30,16 @@ export const galleryImageData =
 		driveId: "1XaoTpV0dKscPtOvrjU6sUuD1Mo-pyQzH"
 	},
 
-	"finite-subdivision-1":
+	"barnsley-fern":
 	{
-		title: "A Finite Subdivision",
+		title: "The Barnsley Fern",
 
-		parameters: "6 vertices, 6 iterations",
+		featured: `Featured in ${jsma} and ${girlsAngle}`,
 
-		featured: `Featured in ${emu}`,
+		appletLink: "barnsley-fern",
 
-		appletLink: "finite-subdivisions",
-
-		driveId: "1ohpmOx1bVGszGolKGxphW4qc15fghyIc"
+		driveId: "1ED5_vAUhFEF0E3fcnY4NOEYJSVf1P7cA"
 	},
-
-	"generalized-julia-set-2":
-	{
-		title: "A Generalized Julia Set",
-
-		parameters: /* html */`
-			Generated from $z^2 - 0.05z^{-2} + c$
-		`,
-
-		featured: `Featured in ${emu}`,
-
-		appletLink: "generalized-julia-sets",
-
-		driveId: "16LcsF7n7w329sO4T-IwWb-p7ykpglNQO"
-	},
-
-	"hitomezashi-pattern-2":
-	{
-		title: "A Hitomezashi Pattern",
-
-		appletLink: "hitomezashi-patterns",
-
-		driveId: "1JkngUmUdSEJvzYMRTTUIa3jy26F0c6m8"
-	},
-
-
 
 	"brownian-tree":
 	{
@@ -186,195 +52,27 @@ export const galleryImageData =
 		driveId: "1BSuYpCW9jQKqu8NOu6f_6omr_LVMXprh"
 	},
 
-	"lyapunov-fractal":
-	{
-		title: "A Lyapunov Fractal",
-
-		parameters: "Generating string <code>AABB</code>",
-
-		appletLink: "lyapunov-fractals",
-
-		driveId: "1Xb5BMuz-iB9f-CN4l5wyZtbCLLOXrz80"
-	},
-
-	"strange-attractor":
-	{
-		title: "A Lorenz Attractor",
-
-		appletLink: "strange-attractors",
-
-		driveId: "1sRCKC-WE8a2iYNW942uKgAJbHVkAYDrQ"
-	},
-
-	"vector-field":
-	{
-		title: "A Vector Field",
-
-		parameters: /* html */`
-			Generated from $\\left( \\sin\\left( \\frac{y}{2.5} \\right), \\tan\\left( \\frac{x}{2.5} \\right) \\right)$
-		`,
-
-		appletLink: "vector-fields",
-
-		driveId: "1DyYo2hLpJ-9NjwW_akmmmq3pmnS9VKPa"
-	},
-
-	"julia-set-1":
-	{
-		title: "A Julia Set",
-
-		appletLink: "julia-set-explorer",
-
-		driveId: "1V4B0ULle-Dq0-ludCNyQg5lQ8Hm6cm_X"
-	},
-
-	"snowflake":
-	{
-		title: "A Gravner-Griffeath Snowflake",
-
-		appletLink: "snowflakes",
-
-		featured: `A prior version of this image was featured in ${emu}.`,
-
-		driveId: "1G4GWlWJvQWlP04IxWpNGmVCfwj0RZnX0"
-	},
-
-
-
-	"quaternionic-julia-set-1":
-	{
-		title: "A Quaternionic Julia Set",
-
-		parameters: /* html */`
-			Generated with $c = (-0.54, -0.25, -0.668, 0)$
-		`,
-
-		featured: `A prior version of this image was featured in ${bridges2023}.`,
-
-		appletLink: "quaternionic-julia-sets",
-
-		driveId: "1x2ss96tGmo-IvT2H31tm1zHimm8p7CWz"
-	},
-
-	"kicked-rotator":
-	{
-		title: "A Kicked Rotator",
-
-		parameters: /* html */`
-			Generated with $K = 0.75$
-		`,
-
-		featured: `A prior version of this image was featured in ${jsma}. The current version was featured in ${emu}.`,
-
-		appletLink: "the-kicked-rotator",
-
-		driveId: "1vXP_8ArZg7F2o7rbFCsksoHDT3nw8y66"
-	},
-
-	"kaleidoscopic-ifs-1":
-	{
-		title: "A Kaleidoscopic IFS Fractal",
-
-		appletLink: "kaleidoscopic-ifs-fractals",
-
-		driveId: "142FfsLHr4aJDbkwH0zPIBFtvtgCqvBgn"
-	},
-
-	"kaleidoscopic-ifs-2":
-	{
-		title: "A Kaleidoscopic IFS Fractal",
-
-		appletLink: "kaleidoscopic-ifs-fractals",
-
-		driveId: "10Z3WG23_IowWw81UnvZ3ABnrJpzCVBbL"
-	},
-
-	"generalized-julia-set-1":
-	{
-		title: "A Generalized Julia Set",
-
-		parameters: /* html */`
-			Generated from $\\sin(cz)$
-		`,
-
-		featured: `Featured in ${emu}`,
-
-		appletLink: "generalized-julia-sets",
-
-		driveId: "1rgAIVr9Ztfs2EiVtY_39-GQ91ayeMJXu"
-	},
-
-	"quasi-fuchsian-group":
-	{
-		title: "A Quasi-Fuchsian Group",
-
-		featured: `A prior version of this image was featured in ${emu}.`,
-
-		appletLink: "quasi-fuchsian-groups",
-
-		driveId: "1mAQCyO3bHIL7yAV1I8uxVFmLBGbhQwRQ"
-	},
-
-	"chaos-game-1":
+	"chaos-game":
 	{
 		title: "A Chaos Game",
 
 		featured: `Featured in ${emu}`,
 
-		appletLink: "the-chaos-game",
+		appletLink: "chaos-game",
 
 		driveId: "15NZyBmIqZxCaTrT9C9m87bqfM1sUmY5i"
 	},
 
-
-
-	"wilsons-algorithm-2":
+	"chaos-game-2":
 	{
-		title: "Wilson&#x2019;s Algorithm",
+		title: "A Chaos Game",
 
-		featured: `A prior version of this image was featured in ${jsma} and ${girlsAngle}. This version was featured in ${emu} and ${bridges2024}.`,
+		appletLink: "chaos-game",
 
-		appletLink: "wilsons-algorithm",
-
-		driveId: "1EJD5tYppybnvAxK1bWpLEXK2gjKAnMqZ"
+		driveId: "14ADChQ4KHV426ySW_CIVvTAXe2irMKU0"
 	},
 
-	
-
-	"barnsley-fern":
-	{
-		title: "The Barnsley Fern",
-
-		featured: `Featured in ${jsma} and ${girlsAngle}`,
-
-		appletLink: "the-barnsley-fern",
-
-		driveId: "1ED5_vAUhFEF0E3fcnY4NOEYJSVf1P7cA"
-	},
-
-	"generalized-julia-set-3":
-	{
-		title: "A Generalized Julia Set",
-
-		parameters: /* html */`
-			Generated from $\\left( \\left| \\operatorname{Re} z \\right| - \\left| \\operatorname{Im} z \\right| \\right)^2 + c$
-		`,
-
-		appletLink: "generalized-julia-sets",
-
-		driveId: "1ZOh2bya7QzuvSQqkM55cLxV3v-0MlET3"
-	},
-
-	"magic-carpet":
-	{
-		title: "A Magic Carpet",
-
-		appletLink: "magic-carpets",
-
-		driveId: "1bD3P4pzIU7M6Ni_lp9oSjtrwoAJQpJ98"
-	},
-
-	"complex-map-1":
+	"complex-map":
 	{
 		title: "A Complex Map",
 
@@ -386,101 +84,6 @@ export const galleryImageData =
 
 		driveId: "1rcF2qy4gGCJ3lg3X8jQW0Z0_OcFYKv8Q"
 	},
-
-	"hopf-fibration":
-	{
-		title: "The Hopf Fibration",
-
-		appletLink: "hopf-fibration",
-
-		driveId: "1h8kmCtONHyUEX28IF2JzFNL-Qgn-TFEX"
-	},
-
-
-	"secant-method":
-	{
-		title: "The Secant Method",
-
-		parameters: /* html */`
-			Generated from the polynomial $z^6 - 1$ with $a = 1.5$
-		`,
-
-		appletLink: "secant-method",
-
-		featured: `Featured in ${emu}`,
-
-		driveId: "1XaNHxtiX5oA9KcLGs8ZSPFJwpDWt4mPk"
-	},
-
-	"juliabulb-1":
-	{
-		title: "A Juliabulb",
-
-		featured: `A prior version of this image was featured in ${emu}.`,
-
-		appletLink: "the-mandelbulb",
-
-		driveId: "16LaSCsDam08PBt_IuvK_EEq7sGWclUOW"
-	},
-
-	"juliabulb-2":
-	{
-		title: "A Juliabulb",
-
-		featured: `A prior version of this image was featured in ${emu}.`,
-
-		appletLink: "the-mandelbulb",
-
-		driveId: "1kz4oy8A82NZsbtm2JcWmpKUEBkMfpjfa"
-	},
-
-	"newtons-method-extended":
-	{
-		title: "Newton&#x2019;s Method, Extended",
-
-		parameters: /* html */`
-			Generated from $\\sin(z)\\left( -\\sin\\left( \\operatorname{Im}(z) \\right) + i\\sin \\left( \\operatorname{Re} z \\right) \\right)$
-		`,
-
-		appletLink: "newtons-method-extended",
-
-		driveId: "1yQCt0FaiBzQKM5h9RKh5akHs3wihil7B"
-	},
-
-	"chaos-game-2":
-	{
-		title: "A Chaos Game",
-
-		appletLink: "the-chaos-game",
-
-		driveId: "14ADChQ4KHV426ySW_CIVvTAXe2irMKU0"
-	},
-
-	"juliabulb-3":
-	{
-		title: "A Juliabulb",
-
-		featured: `A prior version of this image was featured in ${emu}.`,
-
-		appletLink: "the-mandelbulb",
-
-		driveId: "1H1Rn6_RU9SWfFLPqo184EQ_otYxYKAlA"
-	},
-
-
-
-	"mandelbulb":
-	{
-		title: "The Mandelbulb",
-
-		featured: `A prior version of this image was featured in ${jsma}, ${researchAsArt}, and ${bridges2022}. It is currently on display at the Eugene airport.`,
-
-		appletLink: "the-mandelbulb",
-
-		driveId: "1TsrHd6sEB3Wg2mxdFUiZwHxPhUsH7ZJv"
-	},
-
-
 
 	"double-pendulum-fractal":
 	{
@@ -510,8 +113,188 @@ export const galleryImageData =
 		
 		driveId: "1hvfpRt6StCVqoCizzGTMHFrEpfZow4g0"
 	},
+	
+	"finite-subdivision":
+	{
+		title: "A Finite Subdivision",
 
-	"menger-sponge-1":
+		parameters: "6 vertices, 6 iterations",
+
+		featured: `Featured in ${emu}`,
+
+		appletLink: "finite-subdivisions",
+
+		driveId: "1ohpmOx1bVGszGolKGxphW4qc15fghyIc"
+	},
+
+	"generalized-julia-set":
+	{
+		title: "A Generalized Julia Set",
+
+		parameters: /* html */`
+			Generated from $\\sin(cz)$
+		`,
+
+		featured: `Featured in ${emu}`,
+
+		appletLink: "generalized-julia-sets",
+
+		driveId: "1rgAIVr9Ztfs2EiVtY_39-GQ91ayeMJXu"
+	},
+
+	"generalized-julia-set-2":
+	{
+		title: "A Generalized Julia Set",
+
+		parameters: /* html */`
+			Generated from $z^2 - 0.05z^{-2} + c$
+		`,
+
+		featured: `Featured in ${emu}`,
+
+		appletLink: "generalized-julia-sets",
+
+		driveId: "16LcsF7n7w329sO4T-IwWb-p7ykpglNQO"
+	},
+
+	"generalized-julia-set-3":
+	{
+		title: "A Generalized Julia Set",
+
+		parameters: /* html */`
+			Generated from $\\left( \\left| \\operatorname{Re} z \\right| - \\left| \\operatorname{Im} z \\right| \\right)^2 + c$
+		`,
+
+		appletLink: "generalized-julia-sets",
+
+		driveId: "1ZOh2bya7QzuvSQqkM55cLxV3v-0MlET3"
+	},
+
+	"hitomezashi-pattern":
+	{
+		title: "A Hitomezashi Pattern",
+
+		appletLink: "hitomezashi-patterns",
+
+		driveId: "1JkngUmUdSEJvzYMRTTUIa3jy26F0c6m8"
+	},
+
+	"hopf-fibration":
+	{
+		title: "The Hopf Fibration",
+
+		appletLink: "hopf-fibration",
+
+		driveId: "1h8kmCtONHyUEX28IF2JzFNL-Qgn-TFEX"
+	},
+
+	"julia-set":
+	{
+		title: "A Julia Set",
+
+		appletLink: "julia-set-explorer",
+
+		driveId: "1She5ljhYilPIXpiSN80QyZEspWbnz6mu"
+	},
+
+	"juliabulb":
+	{
+		title: "A Juliabulb",
+
+		featured: `A prior version of this image was featured in ${emu}.`,
+
+		appletLink: "the-mandelbulb",
+
+		driveId: "16LaSCsDam08PBt_IuvK_EEq7sGWclUOW"
+	},
+
+	"juliabulb-2":
+	{
+		title: "A Juliabulb",
+
+		featured: `A prior version of this image was featured in ${emu}.`,
+
+		appletLink: "the-mandelbulb",
+
+		driveId: "1kz4oy8A82NZsbtm2JcWmpKUEBkMfpjfa"
+	},
+
+	"juliabulb-3":
+	{
+		title: "A Juliabulb",
+
+		featured: `A prior version of this image was featured in ${emu}.`,
+
+		appletLink: "the-mandelbulb",
+
+		driveId: "1H1Rn6_RU9SWfFLPqo184EQ_otYxYKAlA"
+	},
+
+	"kaleidoscopic-ifs":
+	{
+		title: "A Kaleidoscopic IFS Fractal",
+
+		appletLink: "kaleidoscopic-ifs-fractals",
+
+		driveId: "142FfsLHr4aJDbkwH0zPIBFtvtgCqvBgn"
+	},
+
+	"kaleidoscopic-ifs-2":
+	{
+		title: "A Kaleidoscopic IFS Fractal",
+
+		appletLink: "kaleidoscopic-ifs-fractals",
+
+		driveId: "10Z3WG23_IowWw81UnvZ3ABnrJpzCVBbL"
+	},
+
+	"kicked-rotator":
+	{
+		title: "A Kicked Rotator",
+
+		parameters: /* html */`
+			Generated with $K = 0.75$
+		`,
+
+		featured: `A prior version of this image was featured in ${jsma}. The current version was featured in ${emu}.`,
+
+		appletLink: "kicked-rotator",
+
+		driveId: "1vXP_8ArZg7F2o7rbFCsksoHDT3nw8y66"
+	},
+
+	"lyapunov-fractal":
+	{
+		title: "A Lyapunov Fractal",
+
+		parameters: "Generating string <code>AABB</code>",
+
+		appletLink: "lyapunov-fractals",
+
+		driveId: "1Xb5BMuz-iB9f-CN4l5wyZtbCLLOXrz80"
+	},
+
+	"magic-carpet":
+	{
+		title: "A Magic Carpet",
+
+		appletLink: "magic-carpets",
+
+		driveId: "1bD3P4pzIU7M6Ni_lp9oSjtrwoAJQpJ98"
+	},
+
+	"mandelbulb":
+	{
+		title: "The Mandelbulb",
+
+		featured: `A prior version of this image was featured in ${jsma}, ${researchAsArt}, and ${bridges2022}. It is currently on display at the Eugene airport.`,
+
+		appletLink: "the-mandelbulb",
+
+		driveId: "1TsrHd6sEB3Wg2mxdFUiZwHxPhUsH7ZJv"
+	},
+
+	"menger-sponge":
 	{
 		title: "A Menger Sponge",
 
@@ -527,5 +310,208 @@ export const galleryImageData =
 		appletLink: "menger-sponge",
 
 		driveId: "16fSQRq5Drql7SjmiuNOFvAEnJD7Z_QSe"
-	}
+	},
+
+	"newtons-method":
+	{
+		title: "Newton&#x2019;s Method",
+
+		parameters: /* html */`
+			Roots at $(\\pm 1, 0), (0, \\pm 1.5), (\\pm 1.5, \\pm 1.5)$
+		`,
+
+		featured: `A prior version of this image was featured in ${emu}.`,
+
+		appletLink: "newtons-method",
+
+		driveId: "1T8X5JduKEg8BkakXzGXZzu3CFW_R88i6"
+	},
+
+	"newtons-method-extended":
+	{
+		title: "Newton&#x2019;s Method, Extended",
+
+		parameters: /* html */`
+			Generated from $\\sin(z)\\left( -\\sin\\left( \\operatorname{Im}(z) \\right) + i\\sin \\left( \\operatorname{Re} z \\right) \\right)$
+		`,
+
+		appletLink: "newtons-method-extended",
+
+		driveId: "1yQCt0FaiBzQKM5h9RKh5akHs3wihil7B"
+	},
+
+	"quasi-fuchsian-group":
+	{
+		title: "A Quasi-Fuchsian Group",
+
+		featured: `A prior version of this image was featured in ${emu}.`,
+
+		appletLink: "quasi-fuchsian-groups",
+
+		driveId: "1mAQCyO3bHIL7yAV1I8uxVFmLBGbhQwRQ"
+	},
+
+	"quaternionic-julia-set":
+	{
+		title: "A Quaternionic Julia Set",
+
+		parameters: /* html */`
+			Generated with $c = (-0.54, -0.25, -0.668, 0)$
+		`,
+
+		featured: `A prior version of this image was featured in ${bridges2023}.`,
+
+		appletLink: "quaternionic-julia-sets",
+
+		driveId: "1x2ss96tGmo-IvT2H31tm1zHimm8p7CWz"
+	},
+		
+	"secant-method":
+	{
+		title: "The Secant Method",
+
+		parameters: /* html */`
+			Generated from the polynomial $z^6 - 1$ with $a = 1.5$
+		`,
+
+		appletLink: "newtons-method",
+
+		featured: `Featured in ${emu}`,
+
+		driveId: "1XaNHxtiX5oA9KcLGs8ZSPFJwpDWt4mPk"
+	},
+
+	"snowflake":
+	{
+		title: "A Gravner-Griffeath Snowflake",
+
+		appletLink: "snowflakes",
+
+		featured: `A prior version of this image was featured in ${emu}.`,
+
+		driveId: "1G4GWlWJvQWlP04IxWpNGmVCfwj0RZnX0"
+	},
+
+	"strange-attractor":
+	{
+		title: "A Lorenz Attractor",
+
+		appletLink: "strange-attractors",
+
+		driveId: "1sRCKC-WE8a2iYNW942uKgAJbHVkAYDrQ"
+	},
+
+	"thurston-geometry-e3":
+	{
+		title: "The Thurston Geometry $\\mathbb{E}^3$",
+
+		appletLink: "thurston-geometries",
+
+		driveId: "1XqXEVoe82btwYcTolWTKT3tlsWz_98c0"
+	},
+
+	"thurston-geometry-h2xe":
+	{
+		title: "The Thurston Geometry $\\mathbb{H}^2 \\times \\mathbb{E}$",
+
+		appletLink: "thurston-geometries",
+
+		driveId: "1LCBMWW4n8IcF3_bGo4up4dcO0ISDqH0n"
+	},
+
+	"thurston-geometry-h3":
+	{
+		title: "The Thurston Geometry $\\mathbb{H}^3$",
+
+		appletLink: "thurston-geometries",
+
+		driveId: "1wm59bChDwRgm91lONQR1bVKTCBKGWien"
+	},
+
+	"thurston-geometry-nil":
+	{
+		title: "The Thurston Geometry Nil",
+
+		appletLink: "thurston-geometries",
+
+		driveId: "1-1h44Q9CEVGqyCkkxAQ-ykODsVzWYKwE"
+	},
+
+	"thurston-geometry-s2xe":
+	{
+		title: "The Thurston Geometry $S^2 \\times \\mathbb{E}$",
+
+		appletLink: "thurston-geometries",
+
+		driveId: "1bA57Lhq12oJ1W611UkRjOwuTs8mIXtm-"
+	},
+
+	"thurston-geometry-s3":
+	{
+		title: "The Thurston Geometry $S^3$",
+
+		appletLink: "thurston-geometries",
+
+		driveId: "1yvG5KfAg7qiYjzBDuXDilaSV23X-KyzB"
+	},
+
+	"thurston-geometry-sl2r":
+	{
+		title: "The Thurston Geometry $\\widetilde{\\operatorname{SL}}(2, \\mathbb{R})$",
+
+		appletLink: "thurston-geometries",
+
+		driveId: "1XBJVX2kWVWz1dH06DIAVlDeqr21ei8Ty"
+	},
+
+	"thurston-geometry-sol":
+	{
+		title: "The Thurston Geometry Sol",
+
+		appletLink: "thurston-geometries",
+
+		driveId: "1Pz0IUsM43_-qEXIf27CWzjOkTnMyLXz6"
+	},
+
+	"vector-field":
+	{
+		title: "A Vector Field",
+
+		parameters: /* html */`
+			Generated from $\\left( \\sin\\left( \\frac{y}{2.5} \\right), \\tan\\left( \\frac{x}{2.5} \\right) \\right)$
+		`,
+
+		appletLink: "vector-fields",
+
+		driveId: "1DyYo2hLpJ-9NjwW_akmmmq3pmnS9VKPa"
+	},
+
+	"voronoi-diagram":
+	{
+		title: "A Voronoi Diagram",
+
+		appletLink: "voronoi-diagrams",
+
+		driveId: "1J1dfHbqx1tX1qkkuJF0ZUp-OPlRezEsV"
+	},
+
+	"voronoi-diagram-2":
+	{
+		title: "A Voronoi Diagram",
+
+		appletLink: "voronoi-diagrams",
+
+		driveId: "1TrBXxbjYeHMbznxnT1NiNXWSj8jVBthA"
+	},
+
+	"wilsons-algorithm":
+	{
+		title: "Wilson&#x2019;s Algorithm",
+
+		featured: `A prior version of this image was featured in ${jsma} and ${girlsAngle}. This version was featured in ${emu} and ${bridges2024}.`,
+
+		appletLink: "wilsons-algorithm",
+
+		driveId: "1EJD5tYppybnvAxK1bWpLEXK2gjKAnMqZ"
+	},
 };
