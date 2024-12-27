1f30a341e0ff2c5e34ced1eb63a1ff18208f624d
Cruz Godar <cruz.godar@gmail.com>
2024-12-27 13:54:25 -0800

    Updated 112

--- teaching/uo/112/notes/1-transformations/scripts/index.js
+++ teaching/uo/112/notes/1-transformations/scripts/index.js
@@ -1,6 +1,7 @@
 import { showPage } from "../../../../../../scripts/src/loadPage.js";
 import {
 	createDesmosGraphs,
+	desmosBlack,
 	desmosBlue,
 	desmosGreen,
 	desmosPurple,
@@ -40,6 +41,44 @@ export default function()
 					{ latex: String.raw`f(x) = x^3`, color: desmosRed },
 					{ latex: String.raw`f(x) = x^a`, color: desmosGreen },
 					{ latex: String.raw`a = 4`, sliderBounds: { min: 4, max: 10, step: 1 } },
+				]
+			},
+
+			"negative-power-functions":
+			{
+				bounds: { left: -5, right: 5, bottom: -5, top: 5 },
+
+				expressions:
+				[
+					{ latex: String.raw`f(x) = x^{-1}`, color: desmosPurple },
+					{ latex: String.raw`f(x) = x^{-2}`, color: desmosBlue },
+					{ latex: String.raw`f(x) = x^{-a}`, color: desmosRed },
+					{ latex: String.raw`a = 3`, sliderBounds: { min: 3, max: 10, step: 1 } },
+				]
+			},
+
+			"fractional-power-functions":
+			{
+				bounds: { left: -5, right: 5, bottom: -5, top: 5 },
+
+				expressions:
+				[
+					{ latex: String.raw`f(x) = x^{1/2}`, color: desmosPurple },
+					{ latex: String.raw`f(x) = x^{1/3}`, color: desmosBlue },
+					{ latex: String.raw`f(x) = x^{1/a}`, color: desmosRed },
+					{ latex: String.raw`a = 4`, sliderBounds: { min: 4, max: 10, step: 1 } },
+				]
+			},
+
+			"inverses":
+			{
+				bounds: { left: -5, right: 5, bottom: -5, top: 5 },
+
+				expressions:
+				[
+					{ latex: String.raw`f(x) = x^3`, color: desmosPurple },
+					{ latex: String.raw`f(x) = x^{1/3}`, color: desmosBlue },
+					{ latex: String.raw`y = x`, color: desmosBlack, secret: true, lineStyle: "DASHED" },
 				]
 			}
 		};
