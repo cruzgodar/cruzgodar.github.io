d312b7172ad02cc98d1f449ace8492e54e7301b1
Cruz Godar <cruz.godar@gmail.com>
2025-01-16 14:40:35 -0700

    Updated derivative notation

--- build/htmdl/components/latex.js
+++ build/htmdl/components/latex.js
@@ -6,7 +6,7 @@ export function parseLatex(latex)
 		.replaceAll(/(?<!\\)\\me(?![a-zA-Z])/g, "\\ -\\!\\!=")
 		.replaceAll(/(?<!\\)\\te(?![a-zA-Z])/g, "\\ \\times\\!\\!=")
 
-		// \span, \image, \swap, \re, \im
+		// \span, \image, \swap, \Re, \Im
 		.replaceAll(/(?<!\\)\\(span|image|swap|Re|Im|proj)(?![a-zA-Z])/g, (match, $1) => `\\operatorname{${$1}}`)
 
 		// Matrices: [[1, 2, 3 ; 4, 5, 6 ; 7, 8, 9]]
@@ -26,6 +26,16 @@ export function parseLatex(latex)
 
 		// A left-aligned block: :: a = 1 ; b = 1 ::
 		.replaceAll(/::(.+?)::/g, (match, $1) => `\\begin{array}{l}${$1.replaceAll(/;/g, "\\\\")}\\end{array}`)
+
+		// Leibniz derivatives:  d/dx, dy/dx, d\theta/dx, etc. Note that
+		// \d for manual derivatives is already handled previously.
+		.replaceAll(/d([a-zA-Z]?)\/d([a-zA-Z]?)/g, (match, $1, $2) => `\\frac{\\text{d}${$1}}{\\text{d}${$2}}`)
+		.replaceAll(/d(\\[a-zA-Z]+?)\/d([a-zA-Z]?)/g, (match, $1, $2) => `\\frac{\\text{d}${$1}}{\\text{d}${$2}}`)
+		.replaceAll(/d([a-zA-Z]?)\/d(\\[a-zA-Z]+?)/g, (match, $1, $2) => `\\frac{\\text{d}${$1}}{\\text{d}${$2}}`)
+		.replaceAll(/d(\\[a-zA-Z]+?)\/d(\\[a-zA-Z]+?)/g, (match, $1, $2) => `\\frac{\\text{d}${$1}}{\\text{d}${$2}}`)
+
+		// Manual d derivatives: \d
+		.replaceAll(/(?<!\\)\\d(?![a-zA-Z])/g, "\\text{d}")
 
 		// Bold: **A**
 		.replaceAll(/\*\*(.+?)\*\*/g, (match, $1) => `\\mathbf{${$1}}`)
