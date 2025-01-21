b57c541d26b2cddd1106b2e5bce821bcffc28c1d
Cruz Godar <cruz.godar@gmail.com>
2025-01-21 13:51:17 -0800

    Updated 112

--- teaching/uo/112/notes/4-sine-and-cosine/scripts/index.js
+++ teaching/uo/112/notes/4-sine-and-cosine/scripts/index.js
@@ -33,7 +33,7 @@ export default function()
 
 			referenceTriangle:
 			{
-				bounds: { left: -4, right: 4, bottom: -4, top: 4 },
+				bounds: { left: -3, right: 3, bottom: -3, top: 3 },
 
 				expressions:
 				[
@@ -55,7 +55,38 @@ export default function()
 
 				expressions:
 				[
-					{ latex: String.raw`(-3, 0), (-3 + \cos(\frac{\pi}{6}), 3 + \sin(\frac{\pi}{6})), (3, 0), (-3, 0)`, color: desmosPurple, points: false, lines: true, secret: true },
+					{ latex: String.raw`(-3, 0), (3\cos(\frac{2\pi}{3}), 3\sin(\frac{2\pi}{3})), (3, 0), (-3, 0)`, color: desmosPurple, points: false, lines: true, secret: true },
+				]
+			},
+
+			scaledUpReferenceTriangle:
+			{
+				bounds: { left: -3, right: 3, bottom: -3, top: 3 },
+
+				expressions:
+				[
+					{ latex: String.raw`a = 30`, secret: true, hidden: true },
+
+					{ latex: String.raw`r = 0.25 \left\{ 0 \leq \theta \leq \frac{\pi}{180} a \right\}`, color: desmosPurple, secret: true },
+					{ latex: String.raw`(.5 \cos(\frac{\pi}{360} a), .5 \sin(\frac{\pi}{360} a))`, color: desmosPurple, label: "θ", showLabel: true, labelSize: "large", hidden: true, secret: true },
+					
+					{ latex: String.raw`x^2 + y^2 = 1`, color: desmosBlack },
+
+					{ latex: String.raw`(\cos(\frac{\pi}{180} a), \sin(\frac{\pi}{180} a))`, color: desmosPurple, points: true, secret: true },
+					{ latex: String.raw`(0, 0), (\cos(\frac{\pi}{180} a), \sin(\frac{\pi}{180} a))`, color: desmosPurple, points: false, lines: true, secret: true },
+					{ latex: String.raw`(\cos(\frac{\pi}{180} a), \sin(\frac{\pi}{180} a)), (\cos(\frac{\pi}{180} a), 0)`, color: desmosBlue, points: false, lines: true, lineStyle: "DASHED", secret: true },
+
+					{ latex: String.raw`(0, 0), (3 \cos(\frac{\pi}{180} a), 3 \sin(\frac{\pi}{180} a)), (3 \cos(\frac{\pi}{180} a), 0), (0, 0)`, color: desmosRed, points: false, lines: true, secret: true },
+				]
+			},
+
+			upsideDownTriangle2:
+			{
+				bounds: { left: -4, right: 4, bottom: -3, top: 5 },
+
+				expressions:
+				[
+					{ latex: String.raw`(-3, 0), (3\cos(\frac{2\pi}{3}), 3\sin(\frac{2\pi}{3})), (3, 0), (-3, 0)`, color: desmosPurple, points: false, lines: true, secret: true },
 				]
 			},
 		};
