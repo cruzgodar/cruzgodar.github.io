1e7a23bdffaad95b7d1066c020c3344009421f67
Cruz Godar <cruz.godar@gmail.com>
2025-02-12 11:42:20 -0800

    Updated 112

--- teaching/uo/112/notes/7-non-right-triangles-and-identities/scripts/index.js
+++ teaching/uo/112/notes/7-non-right-triangles-and-identities/scripts/index.js
@@ -1,6 +1,9 @@
 import {
 	createDesmosGraphs,
 	desmosBlack,
+	desmosBlue,
+	desmosPurple,
+	desmosRed,
 	getDesmosSlider,
 	setGetDesmosData
 } from "/scripts/src/desmos.js";
@@ -19,23 +22,50 @@ export default function()
 
 				expressions:
 				[
-					{ latex: raw`x^2 + y^2 = 4`, color: desmosBlack },
+					{ latex: raw`x^2 + y^2 = b^2`, color: desmosBlack, hidden: true },
 
 					...getDesmosSlider({
-						expression: raw`\alpha = 1`,
+						expression: raw`\gamma = 1`,
 						min: 0,
 						max: raw`\pi`,
 						secret: false,
 					}),
 
 					...getDesmosSlider({
-						expression: raw`\beta = 1`,
+						expression: raw`a = 1.5`,
 						min: 0,
-						max: raw`\pi - \alpha`,
+						max: 3,
 						secret: false,
 					}),
 
-					{ latex: raw`\gamma = \pi - \alpha - \beta` },
+					...getDesmosSlider({
+						expression: raw`b = 2.5`,
+						min: 0,
+						max: 3,
+						secret: false,
+					}),
+
+					{ latex: raw`c = \sqrt{a^2 + b^2 - 2ab\cos(\gamma)}`, secret: true },
+
+					{ latex: raw`(0, 0), (a, 0)`, color: desmosRed, points: false, lines: true, secret: true },
+					{ latex: raw`(a, 0), (b\cos(\gamma), b\sin(\gamma))`, color: desmosPurple, points: false, lines: true, secret: true },
+					{ latex: raw`(b\cos(\gamma), b\sin(\gamma)), (0, 0)`, color: desmosBlue, points: false, lines: true, secret: true },
+
+					{ latex: raw`(0, 0), (a, 0), (b\cos(\gamma), b\sin(\gamma))`, color: desmosBlack, secret: true },
+
+					{ latex: raw`(0.4\cos(0.5\gamma), 0.4\sin(0.5\gamma))`, color: desmosPurple, label: "γ", showLabel: true, hidden: true, secret: true },
+
+					{ latex: raw`a_1 = \arctan(-b\sin(\gamma), -b\cos(\gamma))`, secret: true },
+					{ latex: raw`a_2 = \arctan(-b\sin(\gamma), a - b\cos(\gamma))`, secret: true },
+					{ latex: raw`a_3 = \frac{a_1 + a_2}{2}`, secret: true },
+					{ latex: raw`(b\cos(\gamma), b\sin(\gamma)) + (0.4\cos(a_3), 0.4\sin(a_3))`, color: desmosRed, label: "α", showLabel: true, hidden: true, secret: true },
+
+					{ latex: raw`b_3 = \arctan(0.5b\sin(\gamma), 0.5b\cos(\gamma) - a)`, secret: true },
+					{ latex: raw`(a, 0) + (0.35\cos(b_3), 0.35\sin(b_3))`, color: desmosBlue, label: "β", showLabel: true, hidden: true, secret: true },
+
+					{ latex: raw`(0.5a, -0.2)`, color: desmosRed, label: "a", showLabel: true, hidden: true, secret: true },
+					{ latex: raw`(0.5b\cos(\gamma), 0.5b\sin(\gamma)) + (0.2\cos(b_3), 0.2\sin(b_3))`, color: desmosBlue, label: "b", showLabel: true, hidden: true, secret: true },
+					{ latex: raw`(0.5b\cos(\gamma) + 0.5a, 0.5b\sin(\gamma)) + (0.2\cos(0.5\gamma), 0.2\sin(0.5\gamma))`, color: desmosPurple, label: "c", showLabel: true, hidden: true, secret: true },
 				]
 			},
 		};
