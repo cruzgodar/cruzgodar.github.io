7f4f3b3f248426b68d8803fd41163a64d9b9d781
Cruz Godar <cruz.godar@gmail.com>
2025-09-28 21:51:03 -0400

    Updated 118

--- teaching/yale/1180/notes/optimization/scripts/index.js
+++ teaching/yale/1180/notes/optimization/scripts/index.js
@@ -1,5 +1,6 @@
 import {
 	createDesmosGraphs,
+	desmosBlue3d,
 	desmosPurple3d,
 	setGetDesmosData
 } from "/scripts/src/desmos.js";
@@ -13,11 +14,28 @@ export default function()
 		{
 			saddlePoint:
 			{
-				bounds: { xmin: -5, xmax: 5, ymin: -5, ymax: 5, zmin: -1, zmax: 1 },
+				use3d: true,
+
+				bounds: { xmin: -5, xmax: 5, ymin: -5, ymax: 5, zmin: -5, zmax: 5 },
 
 				expressions:
 				[
-					{ latex: raw`f(x, y) = x^3 + y^3`, color: desmosPurple3d },
+					{ latex: raw`f(x, y) = x^2 - y^2`, color: desmosPurple3d },
+				]
+			},
+
+			secretSaddle:
+			{
+				use3d: true,
+
+				bounds: { xmin: -5, xmax: 5, ymin: -5, ymax: 5, zmin: -50, zmax: 50 },
+
+				expressions:
+				[
+					{ latex: raw`f(x, y) = x^2 + y^2 - 3xy`, color: desmosPurple3d },
+
+					{ latex: raw`(t, 0, f(t, 0))`, parametricDomain: { min: -5, max: 5 }, color: desmosBlue3d, secret: true },
+					{ latex: raw`(0, t, f(0, t))`, parametricDomain: { min: -5, max: 5 }, color: desmosBlue3d, secret: true },
 				]
 			},
 		};
