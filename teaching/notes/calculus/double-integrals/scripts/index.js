6794bb743fb8c3520096c1812c3de7917a51e453
Cruz Godar <cruz.godar@gmail.com>
2026-01-19 13:01:19 -0500

    Updated 1200

--- teaching/notes/calculus/double-integrals/scripts/index.js
+++ teaching/notes/calculus/double-integrals/scripts/index.js
@@ -2,9 +2,14 @@ import {
 	createDesmosGraphs,
 	desmosBlack,
 	desmosBlue,
+	desmosBlue3d,
+	desmosGray3d,
 	desmosPurple,
+	desmosPurple3d,
 	desmosRed,
+	desmosRed3d,
 	getDesmosPoint,
+	getDesmosSlider,
 	setGetDesmosData
 } from "/scripts/src/desmos.js";
 import { raw } from "/scripts/src/main.js";
@@ -56,6 +61,141 @@ export default function()
 					{ latex: raw`x = R \{ f(L) \leq y \leq 0 \}`, color: desmosBlue, secret: true }
 				]
 			},
+
+			cartesianProductRectangle:
+			{
+				bounds: { xmin: -1, xmax: 6, ymin: -1, ymax: 6 },
+
+				expressions:
+				[
+					...getDesmosSlider({
+						expression: raw`a = 1`,
+						min: -5,
+						max: 5,
+						secret: false,
+					}),
+					...getDesmosSlider({
+						expression: raw`b = 2`,
+						min: "a",
+						max: 5,
+						secret: false,
+					}),
+					...getDesmosSlider({
+						expression: raw`c = 3`,
+						min: -5,
+						max: 5,
+						secret: false,
+					}),
+					...getDesmosSlider({
+						expression: raw`d = 4`,
+						min: "c",
+						max: 5,
+						secret: false,
+					}),
+
+					{ latex: raw`a \leq x \leq b`, color: desmosBlue, fillOpacity: 0.2, },
+					{ latex: raw`a \leq x \leq b \{ c \leq y \leq d \}`, color: desmosPurple },
+					{ latex: raw`c \leq y \leq d`, color: desmosRed, fillOpacity: 0.2, },
+					{ latex: raw`c \leq y \leq d \{ a \leq x \leq b \}`, color: desmosPurple },
+
+					...getDesmosPoint({
+						point: ["a", "c"],
+						color: desmosBlack,
+						dragMode: "XY",
+						secret: false,
+					}),
+					...getDesmosPoint({
+						point: ["b", "d"],
+						color: desmosBlack,
+						dragMode: "XY",
+						secret: false,
+					}),
+				]
+			},
+
+			riemannSum3d:
+			{
+				use3d: true,
+				
+				bounds: { xmin: -3, xmax: 3, ymin: -3, ymax: 3, zmin: -3, zmax: 3 },
+
+				options: {
+					translucentSurfaces: true,
+				},
+
+				expressions:
+				[
+					{ latex: raw`f(x, y) = \sin(x) + \cos(y) + \frac{(x + y)^3}{100}`, color: desmosPurple3d, fillOpacity: 0.5 },
+
+					...getDesmosSlider({
+						expression: raw`m = 3`,
+						min: 1,
+						max: 15,
+						step: 1,
+						secret: false,
+					}),
+
+					...getDesmosSlider({
+						expression: raw`n = 4`,
+						min: 1,
+						max: 15,
+						step: 1,
+						secret: false,
+					}),
+
+					...getDesmosSlider({
+						expression: raw`a = -1`,
+						min: -5,
+						max: 5,
+						secret: false,
+					}),
+					...getDesmosSlider({
+						expression: raw`b = 2`,
+						min: "a",
+						max: 5,
+						secret: false,
+					}),
+					...getDesmosSlider({
+						expression: raw`c = -2`,
+						min: -5,
+						max: 5,
+						secret: false,
+					}),
+					...getDesmosSlider({
+						expression: raw`d = 2`,
+						min: "c",
+						max: 5,
+						secret: false,
+					}),
+
+					// bounding rectangle
+					{ latex: raw`(t, [c, d], 0)`, color: desmosGray3d, parametricDomain: { min: "a", max: "b" }, },
+					{ latex: raw`([a, b], t, 0)`, color: desmosGray3d, parametricDomain: { min: "c", max: "d" }, },
+
+					{ latex: raw`s_x = \frac{b - a}{m}`, secret: false },
+					{ latex: raw`s_y = \frac{d - c}{n}`, secret: false },
+
+					{ latex: raw`X = [a, a + s_x, ..., b]`, secret: false },
+					{ latex: raw`Y = [c, c + s_y, ..., d]`, secret: false },
+
+					{ latex: raw`L_x = [a, a + s_x, ..., b - s_x]`, secret: false },
+					{ latex: raw`R_x = [a + s_x, a + 2s_x, ..., b]`, secret: false },
+					{ latex: raw`L_y = [c, c + s_y, ..., d - s_y]`, secret: false },
+					{ latex: raw`R_y = [c + s_y, c + 2s_y, ..., d]`, secret: false },
+
+					{ latex: raw`M_x = [a + \frac{s_x}{2}, a + \frac{3s_x}{2}, ..., b - \frac{s_x}{2}]`, secret: false },
+					{ latex: raw`M_y = [c + \frac{s_y}{2}, c + \frac{3s_y}{2}, ..., d - \frac{s_y}{2}]`, secret: false },
+
+					// construct a 1D array of length m*n that contains all the points in M_x x M_y
+
+					{ latex: raw`L = [0, 1, ... mn - 1]`, secret: false },
+					{ latex: raw`I = \floor(\frac{L}{n}) + 1`, secret: false },
+					{ latex: raw`J = \mod(L, n) + 1`, secret: false },
+
+					{ latex: raw`0 \leq z \leq f(M_x[I], M_y[J]) \{ L_x[I] \leq x \leq R_x[I] \} \{ L_y[J] \leq y \leq R_y[J] \}`, color: desmosRed3d, secret: true },
+					{ latex: raw`f(M_x[I], M_y[J]) \leq z \leq 0 \{ L_x[I] \leq x \leq R_x[I] \} \{ L_y[J] \leq y \leq R_y[J] \}`, color: desmosBlue3d, secret: true },
+				]
+			}
 		};
 
 		return data;
