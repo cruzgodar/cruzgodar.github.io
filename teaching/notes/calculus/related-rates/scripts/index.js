65bb67bb260deaed89a9427ad0f0fe807d6e76fd
Cruz Godar <cruz.godar@gmail.com>
2026-01-27 11:32:36 -0500

    Updated 111

--- teaching/notes/calculus/related-rates/scripts/index.js
+++ teaching/notes/calculus/related-rates/scripts/index.js
@@ -1,7 +1,7 @@
 import {
-    createDesmosGraphs,
-    desmosPurple,
-    setGetDesmosData
+	createDesmosGraphs,
+	desmosPurple,
+	setGetDesmosData
 } from "/scripts/src/desmos.js";
 import { raw } from "/scripts/src/main.js";
 
@@ -13,12 +13,12 @@ export default function()
 		{
 			relatedRates:
 			{
-				bounds: { xmin: -10, xmax: 110, ymin: -10, ymax: 110 },
+				bounds: { xmin: -5, xmax: 45, ymin: -10, ymax: 40 },
 
 				expressions:
 				[
-					{ latex: raw`(0, 0), (10t, 100), (10t, 0), (0, 0)`, color: desmosPurple, lines: true },
-					{ latex: raw`t = 10` },
+					{ latex: raw`(0, 0), (10t, 30), (10t, 0), (0, 0)`, color: desmosPurple, lines: true },
+					{ latex: raw`t = 4` },
 				]
 			},
 		};
