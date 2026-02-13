bb21c4e0856365f84303cbbeccadfb807bcae74e
Cruz Godar <cruz.godar@gmail.com>
2026-02-13 10:39:10 -0500

    Updated 120

--- teaching/notes/calculus/greens-theorem/scripts/index.js
+++ teaching/notes/calculus/greens-theorem/scripts/index.js
@@ -1,8 +1,11 @@
 import {
 	createDesmosGraphs,
 	desmosBlack,
+	desmosBlue,
 	desmosPurple,
-	getDesmosSlider
+	desmosRed,
+	getDesmosSlider,
+	getDesmosVector
 } from "/scripts/src/desmos.js";
 import { raw } from "/scripts/src/main.js";
 
@@ -27,6 +30,76 @@ export default function()
 				}),
 
 				{ latex: raw`x = x_0\{ 0 \leq y \leq \sqrt{x_0} \}`, color: desmosPurple },
+			]
+		},
+
+		negativeBoundaryCurve:
+		{
+			bounds: { xmin: -0.25, xmax: 1.25, ymin: -0.25, ymax: 1.25, zmin: -3, zmax: 3 },
+			
+			expressions:
+			[
+				{ latex: raw`f(x) = \sqrt{x}`, hidden: true, secret: true },
+
+				{ latex: raw`y = \sqrt{x}\{ 0 \leq x \leq 1 \}`, color: desmosPurple },
+				{ latex: raw`y = 0\{ 0 \leq x \leq 1 \}`, color: desmosBlue },
+				{ latex: raw`x = 1\{ 0 \leq y \leq 1 \}`, color: desmosRed },
+
+				...getDesmosVector({
+					from: ["0.4", "f(0.4)"],
+					to: ["0.4 + 0.001", "f(0.4) + 0.001f'(0.4)"],
+					color: desmosPurple,
+					arrowSize: "0.05",
+				}),
+
+				...getDesmosVector({
+					from: ["0.5", "0"],
+					to: ["0.5 - 0.001", "0"],
+					color: desmosBlue,
+					arrowSize: "0.05",
+				}),
+
+				...getDesmosVector({
+					from: ["1", "0.5"],
+					to: ["1", "0.5 - 0.001"],
+					color: desmosRed,
+					arrowSize: "0.05",
+				}),
+			]
+		},
+
+		positiveBoundaryCurve:
+		{
+			bounds: { xmin: -0.25, xmax: 1.25, ymin: -0.25, ymax: 1.25, zmin: -3, zmax: 3 },
+			
+			expressions:
+			[
+				{ latex: raw`f(x) = \sqrt{x}`, hidden: true, secret: true },
+
+				{ latex: raw`y = \sqrt{x}\{ 0 \leq x \leq 1 \}`, color: desmosPurple },
+				{ latex: raw`y = 0\{ 0 \leq x \leq 1 \}`, color: desmosBlue },
+				{ latex: raw`x = 1\{ 0 \leq y \leq 1 \}`, color: desmosRed },
+
+				...getDesmosVector({
+					from: ["0.4", "f(0.4)"],
+					to: ["0.4 - 0.001", "f(0.4) - 0.001f'(0.4)"],
+					color: desmosPurple,
+					arrowSize: "0.05",
+				}),
+
+				...getDesmosVector({
+					from: ["0.5", "0"],
+					to: ["0.5 + 0.001", "0"],
+					color: desmosBlue,
+					arrowSize: "0.05",
+				}),
+
+				...getDesmosVector({
+					from: ["1", "0.5"],
+					to: ["1", "0.5 + 0.001"],
+					color: desmosRed,
+					arrowSize: "0.05",
+				}),
 			]
 		},
 	});
