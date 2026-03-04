1dbcd7c99c5e236cf610a86bc313205e66bce768
Cruz Godar <cruz.godar@gmail.com>
2026-03-04 11:02:15 -0500

    Updated 120

--- teaching/notes/calculus/surface-integrals/scripts/index.js
+++ teaching/notes/calculus/surface-integrals/scripts/index.js
@@ -1,5 +1,53 @@
+import { createDesmosGraphs, desmosPurple, getDesmosSlider } from "/scripts/src/desmos.js";
+import { raw } from "/scripts/src/main.js";
 
 export default function()
 {
-	
+	createDesmosGraphs({
+		parametricCurveShaping:
+		{
+			use3d: true,
+
+			options: { showPlane3D: false },
+
+			bounds: { xmin: -1.5, xmax: 1.5, ymin: -1.5, ymax: 1.5, zmin: -1.5, zmax: 1.5 },
+
+			expressions:
+			[
+				{ latex: raw`l(t) = (\cos(2t), \sin(2t), t)`, hidden: true },
+				{ latex: raw`(1 - s) (1, 0, 0)t + s l(6(t-0.5))`, color: desmosPurple, parametricDomain: { min: 0, max: 1 }, secret: true },
+
+				...getDesmosSlider({
+					expression: "s = 0",
+					min: 0,
+					max: 1,
+					secret: false
+				}),
+			]
+		},
+
+		parametricSurfaceShaping:
+		{
+			use3d: true,
+
+			options: { showPlane3D: false },
+
+			bounds: { xmin: -1.5, xmax: 1.5, ymin: -1.5, ymax: 1.5, zmin: -1.5, zmax: 1.5 },
+
+			expressions:
+			[
+				{ latex: raw`X(u) = \tan(\pi (u - \frac{1}{2}))`, hidden: true, secret: false },
+				{ latex: raw`Y(v) = \tan(\pi (v - \frac{1}{2}))`, hidden: true, secret: false },
+				{ latex: raw`c(u, v) = \frac{1}{1 + X(u)^2 + Y(v)^2}(2X(u), 2Y(v), -1 + X(u)^2 + Y(v)^2)`, hidden: true, secret: false },
+				{ latex: raw`(1 - s) (u, v, 0) + s c(u, v)`, color: desmosPurple, parametricDomain3Du: { min: 0, max: 1 }, parametricDomain3Dv: { min: 0, max: 1 }, secret: true },
+
+				...getDesmosSlider({
+					expression: "s = 0",
+					min: 0,
+					max: 1,
+					secret: false
+				}),
+			]
+		},
+	});
 }
